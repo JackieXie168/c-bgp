3b566ac85f7dd6f8aaf38c0cf2b3e617a6c38a9b
nobody <nobody@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2004-04-02 12:57:28 +0000

    This commit was manufactured by cvs2svn to create tag 'CBGP_1_1_3'.

---
+++ tags/CBGP_1_1_3/src/bgp/ecomm_t.h
@@ -0,0 +1,40 @@
+// ==================================================================
+// @(#)ecomm_t.h
+//
+// @author Bruno Quoitin (bqu@infonet.fundp.ac.be)
+// @date 06/12/2002
+// @lastdate 20/08/2003
+// ==================================================================
+
+#ifndef __ECOMM_T_H__
+#define __ECOMM_T_H__
+
+#include <libgds/types.h>
+
+#include <net/prefix.h>
+
+#define ECOMM_RED 3
+
+#define ECOMM_RED_ACTION_PREPEND 0
+#define ECOMM_RED_ACTION_NO_EXPORT 1
+#define ECOMM_RED_ACTION_IGNORE 2
+
+#define ECOMM_RED_FILTER_AS 0
+#define ECOMM_RED_FILTER_2AS 1
+#define ECOMM_RED_FILTER_CIDR 2
+#define ECOMM_RED_FILTER_AS4 3
+
+typedef struct {
+  unsigned char uIANAAuthority:1;
+  unsigned char uTransitive:1;
+  unsigned char uTypeHigh:6;
+  unsigned char uTypeLow;
+  unsigned char auValue[6];
+} SECommunity;
+
+typedef struct {
+  unsigned char uNum;
+  SECommunity asComms[0];
+} SECommunities;
+
+#endif
