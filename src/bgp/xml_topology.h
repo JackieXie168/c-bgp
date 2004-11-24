0e1d32e8f24ac8367d287e0b3868bc550a6f5f79
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2004-11-24 13:23:53 +0000

    Added: HAVE_XML config variable and --enable-xml option for ./configure script.

--- src/bgp/xml_topology.h
+++ src/bgp/xml_topology.h
@@ -2,19 +2,24 @@
 // @(#)xml_topology.h
 //
 // @author Sebastien Tandel (standel@info.ucl.ac.be)
+// @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 08/09/2003
-// @lastdate 15/09/2004
+// @lastdate 24/11/2004
 // ==================================================================
 
 #ifndef __BGP_XML_H__
 #define __BGP_XML_H__
 
+#include <config.h>
 #include <net/network.h>
 
+#ifdef HAVE_XML
 
 // ----- xml_topology_load -------------------------------------------
 int xml_topology_load(char * cFile, SNetwork * pNetwork);
 // ----- xml_topology_dump -------------------------------------------
 int xml_topology_dump(SNetwork * pNetwork, char * pcFile);
+
+#endif
 
 #endif
