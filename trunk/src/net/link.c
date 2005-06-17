46fe328cddc86c5478eeaafa69aef4fb6b0916b5
siasi <siasi@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2005-06-17 09:49:52 +0000

    creation of different types of links towards  - TRANSIT SUBNET  - STUB SUBNET  - ROUTER
    for router we store tAddr to locate Node
    for subnet we store pointer to SNetSubnet structure
    so the IP address of subnet or router MUST be get by link_get_addr()

--- trunk/src/net/link.c
+++ trunk/src/net/link.c
@@ -10,12 +10,63 @@
 #include <config.h>
 #endif
 
+#include <net/subnet.h>
 #include <net/link.h>
+#include <libgds/memory.h>
 
-// ----- link_set_state ---------------------------------------------
-/**
- *
- */
+
+/////////////////////////////////////////////////////////////////////
+// LINK METHODS
+/////////////////////////////////////////////////////////////////////
+// ----- create_link_toRouter ----------------------------------------
+SNetLink * create_link_toRouter(SNetNode * pNode)
+{
+  SNetLink * pLink= (SNetLink *) MALLOC(sizeof(SNetLink));
+  pLink->uDestinationType = NET_LINK_TYPE_ROUTER;
+  (pLink->UDestId).tAddr = pNode->tAddr;   
+  return pLink;
+}
+
+// ----- create_link_toSubnet ----------------------------------------
+SNetLink * create_link_toSubnet(SNetSubnet * pSubnet)
+{
+  SNetLink * pLink= (SNetLink *) MALLOC(sizeof(SNetLink));
+  if (subnet_is_transit(pSubnet))
+    pLink->uDestinationType = NET_LINK_TYPE_TRANSIT;
+  else 
+    pLink->uDestinationType = NET_LINK_TYPE_STUB;
+  (pLink->UDestId).pSubnet = pSubnet;
+  return pLink;
+}
+
+//  ----- create_link_toRouter_byAddr ----------------------------------------
+SNetLink * create_link_toRouter_byAddr(net_addr_t tAddr)
+{
+  SNetLink * pLink= (SNetLink *) MALLOC(sizeof(SNetLink));
+  pLink->uDestinationType = NET_LINK_TYPE_ROUTER;
+  (pLink->UDestId).tAddr = tAddr;
+  return pLink;
+}
+
+// ----- link_get_addr ----------------------------------------
+net_addr_t link_get_addr(SNetLink * pLink)
+{
+  if (pLink->uDestinationType == NET_LINK_TYPE_ROUTER )
+    return (pLink->UDestId).tAddr;
+  else 
+  {
+    SNetSubnet * pSubnet = (pLink->UDestId).pSubnet;
+    return pSubnet->tAddr;
+  }
+}
+
+// ----- link_set_type ---------------------------------------------
+void link_set_type(SNetLink * pLink, uint8_t uDestinationType)
+{
+  pLink->uDestinationType = uDestinationType;
+}
+
+// ----- link_set_state ----------------------------------------
 void link_set_state(SNetLink * pLink, uint8_t uFlag, int iState)
 {
   if (iState)
@@ -51,10 +102,18 @@ uint32_t link_get_igp_weight(SNetLink * pLink)
   return pLink->uIGPweight;
 }
 
+
 // ----- link_dump --------------------------------------------------
 void link_dump(FILE * pStream, SNetLink * pLink)
 {
-  ip_address_dump(pStream, pLink->tAddr);
+  ip_address_dump(pStream, link_get_addr(pLink));
+  if (pLink->uDestinationType == NET_LINK_TYPE_ROUTER)
+    fprintf(pStream, "\tTO ROUTER");
+  else if (pLink->uDestinationType == NET_LINK_TYPE_TRANSIT)
+    fprintf(pStream, "\tTO TRANSIT");
+  else 
+    fprintf(pStream, "\tTO SUBNET");
+    
   fprintf(pStream, "\t%u\t%u", pLink->tDelay, pLink->uIGPweight);
   if (link_get_state(pLink, NET_LINK_FLAG_UP))
     fprintf(pStream, "\tUP");
--- trunk/src/net/link.h
+++ trunk/src/net/link.h
@@ -10,16 +10,23 @@
 #define __NET_LINK_H__
 
 #include <stdlib.h>
-
+#include <net/subnet.h>
 #include <net/message.h>
 #include <net/prefix.h>
 #include <libgds/types.h>
+#include <libgds/array.h>
 
 #define NET_LINK_DELAY_INT
 #define NET_LINK_FLAG_UP      0x01
 #define NET_LINK_FLAG_NOTIFY  0x02
 #define NET_LINK_FLAG_IGP_ADV 0x04
 #define NET_LINK_FLAG_TUNNEL  0x08
+
+// Link type distiguishs between Router, Transit Network and
+// Stub Network to compute IGP route in OSPF
+#define NET_LINK_TYPE_ROUTER   0
+#define NET_LINK_TYPE_TRANSIT  1
+#define NET_LINK_TYPE_STUB     2
 
 #ifdef NET_LINK_DELAY_INT
 typedef uint32_t net_link_delay_t;
@@ -31,15 +38,39 @@ typedef double net_link_delay_t;
 typedef int (*FNetLinkForward)(net_addr_t tDstAddr, void * pContext,
 			       SNetMessage * pMsg);
 
-typedef struct {
+
+/*
+  A link can point to a router or a subnet.
+  Router can be reached in topology db by IP address (tAddr),
+  Subnet can be reached in topology db by memory pointer.
+  
+  To obtain IP address MUST BE USED link_get_addr() function
+*/
+typedef union {
   net_addr_t tAddr;
+  SNetSubnet * pSubnet;
+} UDestinationId;
+
+typedef struct {
+  uint8_t uDestinationType;    /* TNET_LINK_TYPE_ROUTER , NET_LINK_TYPE_TRANSIT, NET_LINK_TYPE_STUB */
+  //net_addr_t tAddr;
+  UDestinationId UDestId;      /* depends on uDestinationType*/
   net_link_delay_t tDelay;
   uint8_t uFlags;
-  uint32_t uIGPweight; // Store IGP weight here
+  uint32_t uIGPweight;      // Store IGP weight here
   void * pContext;
   FNetLinkForward fForward;
 } SNetLink;
 
+
+// ----- link_get_addr ----------------------------------------
+extern net_addr_t link_get_addr(SNetLink * pLink);
+// ----- link_get_addr ----------------------------------------
+extern net_addr_t link_get_addr(SNetLink * pLink);
+// ----- link_set_type ----------------------------------------
+extern void link_set_type(SNetLink * pLink, uint8_t uType);
+// ----- link_get_type ----------------------------------------
+extern int link_check_type(SNetLink * pLink, uint8_t uType);
 // ----- link_set_state ---------------------------------------------
 extern void link_set_state(SNetLink * pLink, uint8_t uFlag, int iState);
 // ----- link_get_state ---------------------------------------------
@@ -50,5 +81,16 @@ extern void link_set_igp_weight(SNetLink * pLink, uint32_t uIGPweight);
 extern uint32_t link_get_igp_weight(SNetLink * pLink);
 // ----- link_dump --------------------------------------------------
 extern void link_dump(FILE * pStream, SNetLink * pLink);
+
+
+
+
+// ----- create_link_Router ----------------------------------------
+extern SNetLink * create_link_toRouter(SNetNode * pNode);
+// ----- create_link_Router ----------------------------------------
+extern SNetLink * create_link_toRouter_byAddr(net_addr_t tAddr);
+// ----- create_link_Subnet ----------------------------------------
+extern SNetLink * create_link_toSubnet(SNetSubnet * pSubnet);
+
 
 #endif
