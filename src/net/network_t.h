f52b3ba54b75be5c19e9b5ca2774bbeb8faaa921
siasi <siasi@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2005-06-20 14:54:31 +0000

    Export node definition type and other in order to be able to resolve include conflicts.

---
+++ src/net/network_t.h
@@ -0,0 +1,45 @@
+// ==================================================================
+// @(#)network_t.h
+//
+// @author Bruno Quoitin (bqu@info.ucl.ac.be)
+// @date 4/07/2003
+// @lastdate 17/05/2005
+// ==================================================================
+
+#ifndef __NET_NETWORK_T_H__
+#define __NET_NETWORK_T_H__
+
+#include <net/routing_t.h>
+#include <net/protocol.h>
+#include <net/domain_t.h>
+
+extern const net_addr_t MAX_ADDR;
+
+typedef struct {
+#ifdef __EXPERIMENTAL__
+  STrie * pNodes;
+#else
+  SRadixTree * pNodes;
+#endif
+  SPtrArray  * pDomains;
+} SNetwork;
+
+typedef struct {
+  net_addr_t tAddr;
+  SPtrArray * aInterfaces;
+  char * pcName;
+  uint32_t  uAS;
+  SUInt32Array * pOSPFAreas; 
+  SNetwork * pNetwork;
+  SPtrArray * pLinks;
+  SNetRT * pRT;
+  SNetProtocols * pProtocols;
+  SNetDomain * pDomain;
+} SNetNode;
+
+// ----- node_links_compare -----------------------------------------
+int node_links_compare(void * pItem1, void * pItem2,
+		       unsigned int uEltSize);
+
+#endif
+
