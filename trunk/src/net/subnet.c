e4d340d2706e975e10386e4c3bd7b298730580fe
siasi <siasi@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2005-06-21 08:57:18 +0000

    FIXED: subnet use ip prefix and not ip address.

--- trunk/src/net/subnet.c
+++ trunk/src/net/subnet.c
@@ -22,10 +22,27 @@
 /////////////////////////////////////////////////////////////////////
 
 // ----- subnet_create ---------------------------------------------------
-SNetSubnet * subnet_create(net_addr_t tAddr, uint8_t uType)
+SNetSubnet * subnet_create(/*net_addr_t tAddr*/SPrefix * pPrefix, uint8_t uType)
 {
   SNetSubnet * pSubnet= (SNetSubnet *) MALLOC(sizeof(SNetSubnet));
-  pSubnet->tAddr= tAddr;
+  pSubnet->pPrefix= pPrefix;//tAddr;
+  pSubnet->uOSPFArea = NO_AREA;
+  pSubnet->uType = uType;
+  
+  pSubnet->aNodes = ptr_array_create(ARRAY_OPTION_SORTED|ARRAY_OPTION_UNIQUE,
+				  node_compare,
+				  NULL);
+  return pSubnet; 
+}
+
+SNetSubnet * subnet_create_byAddr(net_addr_t tNetwork, uint8_t uMaskLen, uint8_t uType)
+{
+  SNetSubnet * pSubnet= (SNetSubnet *) MALLOC(sizeof(SNetSubnet));
+  SPrefix * pPrefix = (SPrefix *) MALLOC(sizeof(SPrefix));
+  pPrefix->tNetwork = tNetwork;
+  pPrefix->uMaskLen = uMaskLen;
+  
+  pSubnet->pPrefix= pPrefix;//tAddr;
   pSubnet->uOSPFArea = NO_AREA;
   pSubnet->uType = uType;
   
@@ -59,9 +76,15 @@ int subnet_link_toNode(SNetSubnet * pSubnet, SNetNode * pNode){
 
 
 // ----- subnet_getAddr --------------------------------------------------
-net_addr_t subnet_get_Addr(SNetSubnet * pSubnet)
+/*net_addr_t subnet_get_Addr(SNetSubnet * pSubnet)
 {
   return pSubnet->tAddr;
+}*/
+
+// ----- subnet_getAddr --------------------------------------------------
+SPrefix * subnet_get_Prefix(SNetSubnet * pSubnet)
+{
+  return pSubnet->pPrefix;
 }
 
 // ----- subnet_is_transit -------------------------------------------------
