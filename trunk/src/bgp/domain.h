9e25d60758feee41704759c372b21b405e38bcc2
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2006-04-10 11:37:54 +0000

    Updated: - use new libgds/log API. - new 'for_each' function.

--- trunk/src/bgp/domain.h
+++ trunk/src/bgp/domain.h
@@ -3,21 +3,28 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 13/02/2002
-// @lastdate 04/08/2005
+// @lastdate 03/03/2006
 // ==================================================================
 
 #ifndef __BGP_DOMAIN_H__
 #define __BGP_DOMAIN_H__
 
+#include <libgds/log.h>
 #include <libgds/radix-tree.h>
 
 #include <bgp/as_t.h>
+
+// -----[ FBGPDomainsForEach ]---------------------------------------
+typedef int (*FBGPDomainsForEach)(SBGPDomain * pDomain, void * pContext);
 
 // ----- bgp_domain_create ------------------------------------------
 extern SBGPDomain * bgp_domain_create(uint16_t uNumber);
 
 // ----- bgp_domain_destroy -----------------------------------------
 extern void bgp_domain_destroy(SBGPDomain ** ppDomain);
+
+// -----[ bgp_domains_for_each ]-------------------------------------
+extern int bgp_domains_for_each(FBGPDomainsForEach fForEach, void * pContext);
 
 // ----- bgp_domain_add_router --------------------------------------
 extern void bgp_domain_add_router(SBGPDomain * pDomain, SBGPRouter * pRouter);
@@ -39,7 +46,7 @@ extern void register_bgp_domain(SBGPDomain * pDomain);
 // ----- bgp_domain_rescan ------------------------------------------
 extern int bgp_domain_rescan(SBGPDomain * pDomain);
 // ----- bgp_domain_dump_recorded_route -----------------------------
-extern int bgp_domain_dump_recorded_route(FILE * pStream,
+extern int bgp_domain_dump_recorded_route(SLogStream * pStream,
 					  SBGPDomain * pDomain, 
 					  SNetDest sDest,
 					  const uint8_t uOptions);
