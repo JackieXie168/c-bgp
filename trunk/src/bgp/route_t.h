e6e4450be83da8d4049911596ef6905eda769a19
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2006-04-10 09:36:41 +0000

    Fixed: - invalid character in @lastdate comment

--- trunk/src/bgp/route_t.h
+++ trunk/src/bgp/route_t.h
@@ -5,13 +5,12 @@
 // @auhtor Sebastien Tandel (standel@info.ucl.ac.be)
 // 
 // @date 20/11/2003
-// @lastdate 28/0é/2006
+// @lastdate 10/04/2006
 // ==================================================================
 
 #ifndef __BGP_ROUTE_T_H__
 #define __BGP_ROUTE_T_H__
 
-//#define __ROUTER_LIST_ENABLE__
 #define __BGP_ROUTE_INFO_DP__
 
 #include <libgds/array.h>
@@ -23,17 +22,9 @@
 #include <bgp/path.h>
 #include <bgp/qos_t.h>
 #include <bgp/route_reflector.h>
+#include <bgp/types.h>
 #include <net/prefix.h>
 #include <net/network.h>
-
-typedef unsigned char origin_type_t;
-#define ROUTE_ORIGIN_IGP 0        // static route
-#define ROUTE_ORIGIN_EGP 1        // route learned through BGP
-#define ROUTE_ORIGIN_INCOMPLETE 2 // route learned through redistribution
-
-extern unsigned long rt_create_count;
-extern unsigned long rt_copy_count;
-extern unsigned long rt_destroy_count;
 
 // Route flags
 #define ROUTE_FLAG_FEASIBLE   0x0001 /* The route is feasible,
@@ -78,18 +69,6 @@ extern unsigned long rt_destroy_count;
 #define ROUTE_FLAG_RR_CLIENT  0x1000 /* The route was learned from an
 					RR client */
 
-// Route DP-Rule: rule of the decision process which has eventually
-// selected the route as best
-#define ROUTE_DP_RULE_NONE      0
-#define ROUTE_DP_RULE_PREF      1
-#define ROUTE_DP_RULE_PATH      2
-#define ROUTE_DP_RULE_MED       3
-#define ROUTE_DP_RULE_ORIGIN    4
-#define ROUTE_DP_RULE_NEXT_HOP  5
-#define ROUTE_DP_RULE_TIE_BREAK 6
-#define ROUTE_DP_RULE_QOS_DELAY 10
-#define ROUTE_DP_RULE_UNKNOWN   255
-
 #define ROUTE_PREF_DEFAULT 100
 #define ROUTE_MED_MISSING  MAX_UINT32_T
 #define ROUTE_MED_DEFAULT  0
@@ -98,41 +77,18 @@ extern unsigned long rt_destroy_count;
 #define ROUTE_SHOW_MRT   1
 
 typedef struct TRoute {
-  SPrefix sPrefix;      // Destination prefix
-  SPeer * pPeer;        // Peer of the route
-  uint16_t uFlags;      // Flags (best, feasible, eligible, ...)
+  SPrefix sPrefix; // Destination prefix
+  SPeer * pPeer;   // Peer of the route
+  uint16_t uFlags; // Flags (best, feasible, eligible, ...)
 #ifdef __BGP_ROUTE_INFO_DP__
-  uint8_t tRank;        // How the route was selected
-                        // (meaningful only if the route is currently best)
+  uint8_t tRank;   // How the route was selected
+                   // (meaningful only if the route is currently best)
 #endif
-
-  /* Attributes */
-  net_addr_t tNextHop;
-  origin_type_t uOriginType;
-  SBGPPath * pASPathRef;
-  SCommunities * pCommunities;
-  uint32_t uLocalPref;
-  uint32_t uMED;
-  SECommunities * pECommunities;
+  SBGPAttr * pAttr;  // Route attributes
 
   /* Route-Reflection attributes */
   net_addr_t * pOriginator;
   SClusterList * pClusterList;
-
-
-  /* QoS attributes (experimental) */
-#ifdef BGP_QOS
-  qos_delay_t tDelay;
-  qos_bandwidth_t tBandwidth;
-  SPtrArray * pAggrRoutes;
-  struct TRoute * pAggrRoute;
-  struct TRoute * pEBGPRoute;
-#endif
-
-#ifdef __ROUTER_LIST_ENABLE__
-  /* Router-List attribute (experimental) */
-  SClusterList * pRouterList;
-#endif
 
 } SRoute;
 
