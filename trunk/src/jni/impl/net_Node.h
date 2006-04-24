f200351101e29414a7b2b04714844f0b607a6b4f
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2006-04-24 13:11:03 +0000

    Fixed: - #include <net/node.h> instead of <net/Node.h>

--- trunk/src/jni/impl/net_Node.h
+++ trunk/src/jni/impl/net_Node.h
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 19/04/2006
-// @lastdate 21/04/2006
+// @lastdate 24/04/2006
 // ==================================================================
 
 #ifndef __JNI_NET_NODE_H__
@@ -12,7 +12,7 @@
 #include <jni_md.h>
 #include <jni.h>
 
-#include <net/Node.h>
+#include <net/node.h>
 #include <jni/headers/be_ac_ucl_ingi_cbgp_net_Node.h>
 
 // -----[ cbgp_jni_new_net_Node ]------------------------------------
