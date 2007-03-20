b2f1f4f109dca32b375804f4d2feeefa1727057e
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-03-20 16:04:14 +0000

    Added:   - global option iExitOnError

--- trunk/src/ui/output.c
+++ trunk/src/ui/output.c
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 01/06/2004
-// @lastdate 02/06/2004
+// @lastdate 16/01/2007
 // ==================================================================
 
 #include <stdio.h>
@@ -12,6 +12,7 @@
 
 int iOptionAutoFlush= 0;
 int iOptionDebug= 0;
+int iOptionExitOnError= 1;
 
 // ----- flushir ----------------------------------------------------
 /** Flush if required */
--- trunk/src/ui/output.h
+++ trunk/src/ui/output.h
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 01/06/2004
-// @lastdate 18/03/2005
+// @lastdate 16/01/2007
 // ==================================================================
 
 #ifndef __UI_OUTPUT_H__
@@ -13,9 +13,18 @@
 
 extern int iOptionAutoFlush;
 extern int iOptionDebug;
+extern int iOptionExitOnError;
 
-// ----- flushir ----------------------------------------------------
-/** Flush if required */
-extern void flushir(FILE * pStream);
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+  // ----- flushir --------------------------------------------------
+  /** Flush if required */
+  void flushir(FILE * pStream);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __UI_OUTPUT_H__ */
