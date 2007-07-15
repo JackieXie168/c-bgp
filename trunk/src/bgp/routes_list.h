35531fcb8a9377e2132610f4f63dc35e4970a1a1
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-07-15 14:18:46 +0000

    Changes: - header code updated to be integratable in C++ code

--- trunk/src/bgp/routes_list.h
+++ trunk/src/bgp/routes_list.h
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 20/02/2004
-// @lastdate 03/03/2006
+// @lastdate 13/07/2007
 // ==================================================================
 
 #ifndef __ROUTES_LIST_H__
@@ -15,22 +15,30 @@
 
 typedef SPtrArray SRoutes;
 
-// ----- routes_list_create -----------------------------------------
-extern SRoutes * routes_list_create(uint8_t uOptions);
-// ----- routes_list_destroy ----------------------------------------
-extern void routes_list_destroy(SRoutes ** ppRoutes);
-// ----- routes_list_append -----------------------------------------
-extern void routes_list_append(SRoutes * pRoutes, SRoute * pRoute);
-// ----- routes_list_remove_at --------------------------------------
-extern void routes_list_remove_at(SRoutes * pRoutes, int iIndex);
-// ----- routes_list_get_at -----------------------------------------
-SRoute * routes_list_get_at(SRoutes * pRoutes, const int iIndex);
-// ----- routes_list_get_num ----------------------------------------
-extern int routes_list_get_num(SRoutes * pRoutes);
-// ----- routes_list_dump -------------------------------------------
-extern void routes_list_dump(SLogStream * pStream, SRoutes * pRoutes);
-// -----[ routes_list_for_each ]-------------------------------------
-extern int routes_list_for_each(SRoutes * pRoutes, FArrayForEach fForEach,
-				void * pContext);
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+  // ----- routes_list_create ---------------------------------------
+  SRoutes * routes_list_create(uint8_t uOptions);
+  // ----- routes_list_destroy --------------------------------------
+  void routes_list_destroy(SRoutes ** ppRoutes);
+  // ----- routes_list_append ---------------------------------------
+  void routes_list_append(SRoutes * pRoutes, SRoute * pRoute);
+  // ----- routes_list_remove_at ------------------------------------
+  void routes_list_remove_at(SRoutes * pRoutes, int iIndex);
+  // ----- routes_list_get_at ---------------------------------------
+  SRoute * routes_list_get_at(SRoutes * pRoutes, const int iIndex);
+  // ----- routes_list_get_num --------------------------------------
+  int routes_list_get_num(SRoutes * pRoutes);
+  // ----- routes_list_dump -----------------------------------------
+  void routes_list_dump(SLogStream * pStream, SRoutes * pRoutes);
+  // -----[ routes_list_for_each ]-----------------------------------
+  int routes_list_for_each(SRoutes * pRoutes, FArrayForEach fForEach,
+			   void * pContext);
+  
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ROUTES_LIST_H__ */
