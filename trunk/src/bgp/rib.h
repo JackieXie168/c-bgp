954888e216a7d66bb9737aeb0d219c511871f131
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-07-15 14:15:03 +0000

    Changes: - old radix-tree based code removed - experimental trie-based code is no validated

--- trunk/src/bgp/rib.h
+++ trunk/src/bgp/rib.h
@@ -5,11 +5,11 @@
 // @author Sebastien Tandel (standel@info.ucl.ac.be)
 // 
 // @date 04/12/2002
-// @lastdate 28/02/2006
+// @lastdate 21/05/2007
 // ==================================================================
 
-#ifndef __RIB_H__
-#define __RIB_H__
+#ifndef __BGP_RIB_H__
+#define __BGP_RIB_H__
 
 #include <net/prefix.h>
 #include <bgp/rib_t.h>
@@ -17,38 +17,45 @@
 
 #define RIB_OPTION_ALIAS 0x01
 
-// ----- rib_create -------------------------------------------------
-extern SRIB * rib_create(uint8_t uOptions);
-// ----- rib_destroy ------------------------------------------------
-extern void rib_destroy(SRIB ** ppRIB);
-// ----- rib_find_best ----------------------------------------------
-#if defined __EXPERIMENTAL__ && __EXPERIMENTAL_WALTON__
-extern SRoutes * rib_find_best(SRIB * pRIB, SPrefix sPrefix);
-extern SRoute * rib_find_one_best(SRIB * pRIB, SPrefix sPrefix);
-#else
-extern SRoute * rib_find_best(SRIB * pRIB, SPrefix sPrefix);
+#ifdef __cplusplus
+extern "C" {
 #endif
-// ----- rib_find_exact ---------------------------------------------
-#if defined __EXPERIMENTAL__ && __EXPERIMENTAL_WALTON__
-extern SRoutes * rib_find_exact(SRIB * pRIB, SPrefix sPrefix);
-extern SRoute * rib_find_one_exact(SRIB * pRIB, SPrefix sPrefix,  
-					    net_addr_t *tNextHop);
-#else
-extern SRoute * rib_find_exact(SRIB * pRIB, SPrefix sPrefix);
-#endif
-// ----- rib_add_route ----------------------------------------------
-extern int rib_add_route(SRIB * pRIB, SRoute * pRoute);
-// ----- rib_replace_route ------------------------------------------
-extern int rib_replace_route(SRIB * pRIB, SRoute * pRoute);
-// ----- rib_remove_route -------------------------------------------
-#if defined __EXPERIMENTAL__ && __EXPERIMENTAL_WALTON__
-extern int rib_remove_route(SRIB * pRIB, SPrefix sPrefix,
-				net_addr_t * tNextHop);
-#else
-extern int rib_remove_route(SRIB * pRIB, SPrefix sPrefix);
-#endif
-// ----- rib_for_each -----------------------------------------------
-extern int rib_for_each(SRIB * pRIB, FRadixTreeForEach fForEach,
-			void * pContext);
 
+  // ----- rib_create -----------------------------------------------
+  SRIB * rib_create(uint8_t uOptions);
+  // ----- rib_destroy ----------------------------------------------
+  void rib_destroy(SRIB ** ppRIB);
+  // ----- rib_find_best --------------------------------------------
+#if defined __EXPERIMENTAL__ && __EXPERIMENTAL_WALTON__
+  SRoutes * rib_find_best(SRIB * pRIB, SPrefix sPrefix);
+  SRoute * rib_find_one_best(SRIB * pRIB, SPrefix sPrefix);
+#else
+  SRoute * rib_find_best(SRIB * pRIB, SPrefix sPrefix);
 #endif
+  // ----- rib_find_exact -------------------------------------------
+#if defined __EXPERIMENTAL__ && __EXPERIMENTAL_WALTON__
+  SRoutes * rib_find_exact(SRIB * pRIB, SPrefix sPrefix);
+  SRoute * rib_find_one_exact(SRIB * pRIB, SPrefix sPrefix,
+			      net_addr_t *tNextHop);
+#else
+  SRoute * rib_find_exact(SRIB * pRIB, SPrefix sPrefix);
+#endif
+  // ----- rib_add_route --------------------------------------------
+  int rib_add_route(SRIB * pRIB, SRoute * pRoute);
+  // ----- rib_replace_route ----------------------------------------
+  int rib_replace_route(SRIB * pRIB, SRoute * pRoute);
+  // ----- rib_remove_route -----------------------------------------
+#if defined __EXPERIMENTAL__ && __EXPERIMENTAL_WALTON__
+  int rib_remove_route(SRIB * pRIB, SPrefix sPrefix, net_addr_t * tNextHop);
+#else
+  int rib_remove_route(SRIB * pRIB, SPrefix sPrefix);
+#endif
+  // ----- rib_for_each ---------------------------------------------
+  int rib_for_each(SRIB * pRIB, FRadixTreeForEach fForEach,
+		   void * pContext);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __BGP_RIB_H__ */
