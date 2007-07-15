3193ab71e3698afa18affcf84c79f54961837a09
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-07-15 14:10:52 +0000

    Changes: - new programming interface for loading AS-level topologies in the Subramanian format (see as-level.c)

--- src/bgp/rexford.h
+++ src/bgp/rexford.h
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 28/07/2003
-// @lastdate 15/01/2007
+// @lastdate 03/05/2007
 // ==================================================================
 
 #ifndef __BGP_REXFORD_H__
@@ -11,6 +11,7 @@
 
 #include <libgds/log.h>
 
+#include <bgp/as-level.h>
 #include <net/prefix.h>
 #include <net/network.h>
 #include <stdio.h>
@@ -19,51 +20,17 @@
 #define REXFORD_REL_PEER_PEER 0
 #define REXFORD_REL_PROV_CUST 1
 
-// ----- Error codes ----
-#define REXFORD_SUCCESS 0
-#define REXFORD_ERROR_UNEXPECTED       -1
-#define REXFORD_ERROR_OPEN             -2
-#define REXFORD_ERROR_NUM_PARAMS       -3
-#define REXFORD_ERROR_INVALID_ASNUM    -4
-#define REXFORD_ERROR_INVALID_RELATION -5
-#define REXFORD_ERROR_INVALID_DELAY    -6
-#define REXFORD_ERROR_DUPLICATE_LINK   -7
-
-// ----- Marking communities -----
-#define COMM_PROV 1
-#define COMM_PEER 10
-
-// ----- Local preferences
-#define PREF_PROV 60
-#define PREF_PEER 80
-#define PREF_CUST 100
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-  // ----- rexford_get_as -------------------------------------------
-  SBGPRouter * rexford_get_as(uint16_t uASNum);
-  // ----- rexford_load ---------------------------------------------
-  int rexford_load(char * pcFileName);
-  // ----- rexford_setup_policies -----------------------------------
-  void rexford_setup_policies();
-  // ----- rexford_run ----------------------------------------------
-  int rexford_run();
-  // ----- rexford_record_route -------------------------------------
-  int rexford_record_route(SLogStream * pStream, char * pcFileName,
-				  SPrefix sPrefix);
-  // ----- rexford_record_route_bm ----------------------------------
-  int rexford_record_route_bm(SLogStream * pStream, char * pcFileName,
-				     SPrefix sPrefix, uint8_t uBound);
-  // ----- _rexford_init --------------------------------------------
-  void _rexford_init();
-  // ----- _rexford_destroy -----------------------------------------
-  void _rexford_destroy();
+  // -----[ rexford_parser ]-----------------------------------------
+  int rexford_parser(FILE * pStream, SASLevelTopo * pTopo,
+		     unsigned int * puLineNumber);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif /* __BGP_REXFORD_H__ */
 
