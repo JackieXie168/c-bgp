7bedfc2fa7612baaa5c8ad4394ff2190544190f5
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-07-15 14:15:34 +0000

    Changes: - trie is now validated basis for RIB instead of radix-tree

--- src/bgp/rib_t.h
+++ src/bgp/rib_t.h
@@ -3,22 +3,14 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 04/12/2002
-// @lastdate 05/04/2005
+// @lastdate 21/05/2007
 // ==================================================================
 
 #ifndef __RIB_T_H__
 #define __RIB_T_H__
 
-#ifdef __EXPERIMENTAL__
-# include <libgds/patricia-tree.h>
-#else
-# include <libgds/radix-tree.h>
-#endif
+#include <libgds/patricia-tree.h>
 
-#ifdef __EXPERIMENTAL__
 typedef STrie SRIB;
-#else
-typedef SRadixTree SRIB;
-#endif
 
 #endif
