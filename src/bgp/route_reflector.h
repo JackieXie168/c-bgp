4f2d7693be817901b3cfcea63ed10817642b5cca
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-07-26 21:00:36 +0000

    Fixes: - cluster_list_append() is now a function, not a macro

--- src/bgp/route_reflector.h
+++ src/bgp/route_reflector.h
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 23/12/2003
-// @lastdate 28/09/2006
+// @lastdate 23/07/2007
 // ==================================================================
 
 #ifndef __BGP_ROUTE_REFLECTOR_H__
@@ -22,31 +22,40 @@ typedef uint32_t cluster_id_t;
 
 // A CLUSTER_ID_LIST is the list of clusters traversed by a route (RFC1966)
 typedef SUInt32Array SClusterList;
-// ---- cluster_list_create -----------------------------------------
-#define cluster_list_create() \
-          (SClusterList *) uint32_array_create(0)
-// ---- cluster_list_append -----------------------------------------
-#define cluster_list_append(PL, C) \
-          _array_append((SArray *) PL, &C)
-// ----- cluster_list_copy ------------------------------------------
-#define cluster_list_copy(PL) \
-          (SClusterList *) _array_copy((SArray *) PL)
-// ----- cluster_list_length ----------------------------------------
-#define cluster_list_length(PL) \
-          _array_length((SArray *) PL)
 
-// ----- cluster_list_destroy ---------------------------------------
-extern void cluster_list_destroy(SClusterList ** ppClusterList);
-// ----- cluster_list_dump ------------------------------------------
-extern void cluster_list_dump(SLogStream * pStream, SClusterList * pClusterList);
-// ----- cluster_list_equals -------------------------------------------
-extern int cluster_list_equals(SClusterList * pClusterList1,
-			       SClusterList * pClusterList2);
-// ----- cluster_list_contains --------------------------------------
-extern int cluster_list_contains(SClusterList * pClusterList,
-				 cluster_id_t tClusterID);
-
-// -----[ originator_equals ]----------------------------------------
-extern int originator_equals(net_addr_t * pOrig1, net_addr_t * pOrig2);
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+  // ---- cluster_list_create ---------------------------------------
+#define cluster_list_create()				\
+  (SClusterList *) uint32_array_create(0)
+  // ---- cluster_list_append ---------------------------------------
+  inline int cluster_list_append(SClusterList * pClusterList,
+				 cluster_id_t tClusterID);
+  // ----- cluster_list_copy ------------------------------------------
+#define cluster_list_copy(PL)				\
+  (SClusterList *) _array_copy((SArray *) PL)
+  // ----- cluster_list_length ----------------------------------------
+#define cluster_list_length(PL)			\
+  _array_length((SArray *) PL)
+  
+  // ----- cluster_list_destroy -------------------------------------
+  void cluster_list_destroy(SClusterList ** ppClusterList);
+  // ----- cluster_list_dump ----------------------------------------
+  void cluster_list_dump(SLogStream * pStream, SClusterList * pClusterList);
+  // ----- cluster_list_equals --------------------------------------
+  int cluster_list_equals(SClusterList * pClusterList1,
+			  SClusterList * pClusterList2);
+  // ----- cluster_list_contains ------------------------------------
+  int cluster_list_contains(SClusterList * pClusterList,
+			    cluster_id_t tClusterID);
+
+  // -----[ originator_equals ]--------------------------------------
+  int originator_equals(net_addr_t * pOrig1, net_addr_t * pOrig2);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __BGP_ROUTE_REFLECTOR_H__ */
