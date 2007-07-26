d6551e26cc574852fa8a22bb8b83e2ea088e42ff
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-07-26 20:44:01 +0000

    Fix: - comm_cmp() provided -> replace comm comparison in comm_hash - comm_to_string() revised - comm_from_string() imported from mrtd.c - comm_value_from-string() revised

--- src/bgp/comm.c
+++ src/bgp/comm.c
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 23/05/2003
-// @lastdate 28/09/2006
+// @lastdate 22/07/2007
 // ==================================================================
 
 #ifdef HAVE_CONFIG_H
@@ -11,11 +11,15 @@
 #endif
 
 #include <libgds/memory.h>
+#include <libgds/str_util.h>
+#include <libgds/tokenizer.h>
 
 #include <stdlib.h>
 #include <string.h>
 
 #include <comm.h>
+
+static STokenizer * pCommTokenizer= NULL;
 
 // ----- comm_create ------------------------------------------------
 /**
@@ -114,19 +118,22 @@ void comm_remove(SCommunities ** ppComms, comm_t tComm)
  * attribute.
  *
  * Return value:
- *   >= 0   the community was found and return value is the index.
- *   -1     the given community could not be found.
+ *   1  the community was found and return value is the index.
+ *   0  otherwise
  */
 int comm_contains(SCommunities * pComms, comm_t tComm)
 {
   int iIndex= 0;
 
+  if (pComms == NULL)
+    return 0;
+
   while (iIndex < pComms->uNum) {
     if (pComms->asComms[iIndex] == tComm)
-      return iIndex;
+      return 1;
     iIndex++;
   }
-  return -1;
+  return 0;
 }
 
 // ----- comm_equals ------------------------------------------------
@@ -145,11 +152,11 @@ int comm_equals(SCommunities * pComms1, SCommunities * pComms2)
 		 pComms1->uNum*sizeof(comm_t)) == 0);
 }
 
-// ----- comm_from_string -------------------------------------------
+// -----[ comm_value_from_string ]-----------------------------------
 /**
  *
  */
-int comm_from_string(char * pcComm, comm_t * pCommunity)
+int comm_value_from_string(char * pcComm, comm_t * pCommunity)
 {
   unsigned long ulComm;
   char * pcComm2;
@@ -163,28 +170,42 @@ int comm_from_string(char * pcComm, comm_t * pCommunity)
     *pCommunity= COMM_DEPREF;
 #endif
   } else {
-    *pCommunity= 0;
-    ulComm= strtoul(pcComm, &pcComm2, 0);
-    if (*pcComm2 == ':') {
+
+    if (strchr(pcComm, ':') == NULL) {
+
+      // Single part (0-UINT32_MAX)
+      if ((str_as_ulong(pcComm, &ulComm) < 0) ||
+	  (ulComm > UINT32_MAX))
+	return -1;
+
+      *pCommunity= ulComm;
+
+    } else {
+      
+      // Two parts (0-65535):(0-65535)
+      ulComm= strtoul(pcComm, &pcComm2, 0);
+      if (*pcComm2 != ':')
+	return -1;
       if (ulComm > 65535)
 	return -1;
       *pCommunity= ulComm << 16;
       ulComm= strtoul(pcComm2+1, &pcComm2, 0);
+      if (*pcComm2 != '\0')
+	return -1;
+      if (ulComm > 65535)
+	return -1;
+      *pCommunity+= ulComm;
+
     }
-    if (*pcComm2 != 0)
-      return -1;
-    if (ulComm > 65535)
-      return -1;
-    *pCommunity+= ulComm;
   }
   return 0;
 }
 
-// ----- comm_dump2 -------------------------------------------------
+// -----[ comm_value_dump ]------------------------------------------
 /**
  *
  */
-void comm_dump2(SLogStream * pStream, comm_t tCommunity, int iText)
+void comm_value_dump(SLogStream * pStream, comm_t tCommunity, int iText)
 {
   if (iText == COMM_DUMP_TEXT) {
     switch (tCommunity) {
@@ -217,7 +238,7 @@ void comm_dump(SLogStream * pStream, SCommunities * pComms,
     if (iIndex > 0)
       log_printf(pStream, " ");
     tCommunity= (comm_t) pComms->asComms[iIndex];
-    comm_dump2(pStream, tCommunity, iText);
+    comm_value_dump(pStream, tCommunity, iText);
   }
 }
 
@@ -229,27 +250,125 @@ void comm_dump(SLogStream * pStream, SCommunities * pComms,
  * size.
  *
  * Return value:
- *   The function returns a valid pointer if the Communities could be
- *   written in the provided buffer. The returned pointer is the first
- *   character that follows the conversion. If there was not enough
- *   space in the buffer to write the Communities, then NULL is
- *   returned.
+ *   -1    not enough space in destination buffer
+ *   >= 0  number of characters written
  */
 int comm_to_string(SCommunities * pComms, char * pBuffer,
 		   size_t tBufferSize)
 {
   size_t tInitialSize= tBufferSize;
-  int iWritten;
-  int iIndex;
+  int iWritten= 0;
+  unsigned int uIndex;
   comm_t tCommunity;
-  
-  for (iIndex= 0; iIndex < pComms->uNum; iIndex++) {
-    tCommunity= (comm_t) pComms->asComms[iIndex];
-    iWritten= snprintf(pBuffer, tBufferSize, "%ul", (unsigned int) tCommunity);
-    if (iWritten == tBufferSize)
-      return tInitialSize;
+
+  if ((pComms == NULL) || (pComms->uNum == 0)) {
+    if (tBufferSize < 1)
+      return -1;
+    pBuffer[0]= '\0';
+    return 0;
+  }
+ 
+  for (uIndex= 0; uIndex < pComms->uNum; uIndex++) {
+    tCommunity= (comm_t) pComms->asComms[uIndex];
+
+    if (uIndex > 0) {
+      iWritten= snprintf(pBuffer, tBufferSize, " %u", (uint32_t) tCommunity);
+    } else {
+      iWritten= snprintf(pBuffer, tBufferSize, "%u", (uint32_t) tCommunity);
+    }
+    if (iWritten >= tBufferSize)
+      return -1;
     tBufferSize-= iWritten;
     pBuffer+= iWritten;
   }
   return tInitialSize-tBufferSize;
+}
+
+// -----[ comm_from_string ]-----------------------------------------
+/**
+ *
+ */
+SCommunities * comm_from_string(const char * pcCommunities)
+{
+  int iIndex;
+  SCommunities * pComm= NULL;
+  STokens * pTokens;
+  comm_t uComm;
+
+  if (pCommTokenizer == NULL)
+    pCommTokenizer= tokenizer_create(" ", 0, NULL, NULL);
+
+  if (tokenizer_run(pCommTokenizer, (char *) pcCommunities)
+      != TOKENIZER_SUCCESS)
+    return NULL;
+
+  pTokens= tokenizer_get_tokens(pCommTokenizer);
+
+  pComm= comm_create();
+  for (iIndex= 0; iIndex < tokens_get_num(pTokens); iIndex++) {
+    if (comm_value_from_string(tokens_get_string_at(pTokens, iIndex),
+			       &uComm)) {
+      comm_destroy(&pComm);
+      return NULL;
+    }
+    comm_add(&pComm, uComm);
+  }
+  
+  return pComm;
+}
+
+// -----[ comm_cmp ]-------------------------------------------------
+/**
+ * Compare two Communities. This is just used for storing different
+ * communities in a sorted array, not for testing that two Communities
+ * are equal. Indeed, individual community values are not sorted in the
+ * Communities which means that this function can say that two
+ * Communities are different even if they contain the same set of
+ * community values, but in different orderings.
+ *
+ * Return value:
+ *   0  communities are equal
+ *   <0 comm1 < comm2
+ *   >0 comm1 > comm2
+ */
+int comm_cmp(SCommunities * pComm1, SCommunities * pComm2)
+{
+  unsigned int uIndex;
+
+  // Pointers are equal means Communities are equal
+  if (pComm1 == pComm2)
+    return 0;
+
+  // Empty and null Communities are equal
+  if (((pComm1 == NULL) && (pComm2->uNum == 0)) ||
+      ((pComm2 == NULL) && (pComm1->uNum == 0)))
+    return 0;
+
+  if (pComm1 == NULL)
+    return -1;
+  if (pComm2 == NULL)
+    return 1;
+
+  // Check Communities length
+  if (pComm1->uNum < pComm2->uNum)
+    return -1;
+  else if (pComm1->uNum > pComm2->uNum)
+    return 1;
+
+  // Check individual community values
+  for (uIndex= 0; uIndex < pComm1->uNum;uIndex++) {
+    if (pComm1->asComms[uIndex] < pComm2->asComms[uIndex])
+      return -1;
+    else if (pComm1->asComms[uIndex] > pComm2->asComms[uIndex])
+      return 1;
+  }
+
+  // Both Communities are equal
+  return 0;
+}
+
+// -----[ _comm_destroy ]--------------------------------------------
+void _comm_destroy()
+{
+  tokenizer_destroy(&pCommTokenizer);
 }
