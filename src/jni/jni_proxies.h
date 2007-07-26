5977dec6f3719866f177322e75935282c02299e5
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-07-26 21:12:35 +0000

    Fixes: - do not assume that java hashcodes are unique (this is false)

--- src/jni/jni_proxies.h
+++ src/jni/jni_proxies.h
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 27/03/2006
-// @lastdate 25/04/2006
+// @lastdate 29/06/2007
 // ==================================================================
 
 #ifndef __JNI_PROXIES_H__
@@ -11,13 +11,31 @@
 
 #include <jni.h>
 
-// -----[ jni_proxy_add ]--------------------------------------------
-extern void jni_proxy_add(JNIEnv * jEnv, jobject joObject, void * pObject);
-// -----[ jni_proxy_remove ]-----------------------------------------
-extern void jni_proxy_remove(JNIEnv * jEnv, jobject joObject);
-// -----[ jni_proxy_lookup ]-----------------------------------------
-extern void * jni_proxy_lookup(JNIEnv * jEnv, jobject joObject);
-// -----[ jni_proxy_get_CBGP ]---------------------------------------
-extern jobject jni_proxy_get_CBGP(JNIEnv * jEnv, jobject joObject);
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+  // -----[ jni_proxy_add ]------------------------------------------
+  void jni_proxy_add(JNIEnv * jEnv, jobject joObject, void * pObject);
+  // -----[ jni_proxy_remove ]---------------------------------------
+  void jni_proxy_remove(JNIEnv * jEnv, jobject joObject);
+  // -----[ jni_proxy_lookup ]---------------------------------------
+  void * jni_proxy_lookup(JNIEnv * jEnv, jobject joObject);
+  // -----[ jni_proxy_get_CBGP ]-------------------------------------
+  jobject jni_proxy_get_CBGP(JNIEnv * jEnv, jobject joObject);
+
+  // -----[ jni_init_lock ]------------------------------------------
+  void jni_init_lock(jobject joObj);
+  // -----[ jni_lock ]-----------------------------------------------
+  void jni_lock(JNIEnv * jEnv);
+  // -----[ jni_unlock ]---------------------------------------------
+  void jni_unlock(JNIEnv * jEnv);
+
+#define return_jni_unlock(E, R) if (1) { jni_unlock(E); return R; }
+#define return_jni_unlock2(E) if (1) { jni_unlock(E); return; }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __JNI_PROXIES_H__ */
