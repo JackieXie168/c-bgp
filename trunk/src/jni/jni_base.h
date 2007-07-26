1b9481ba59113e85b307e560138e7cea5bf83f88
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-07-26 21:05:25 +0000

    Change: - use extern "C" {} if compiled by c++ compiler

--- trunk/src/jni/jni_base.h
+++ trunk/src/jni/jni_base.h
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 21/03/2006
-// @lastdate 27/03/2006
+// @lastdate 01/06/2007
 // ==================================================================
 
 #ifndef __JNI_BASE_H__
@@ -11,59 +11,67 @@
 
 #include <jni.h>
 
-/////////////////////////////////////////////////////////////////////
-// Helper functions for Java objects creation and method calls
-/////////////////////////////////////////////////////////////////////
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-// -----[ cbgp_jni_new ]---------------------------------------------
-extern jobject cbgp_jni_new(JNIEnv * env, const char * pcClass,
-			    const char * pcConstr, ...);
-// -----[ cbgp_jni_call_void ]---------------------------------------
-extern int cbgp_jni_call_void(JNIEnv * env, jobject joObject,
-			      const char * pcMethod,
-			      const char * pcSignature, ...);
-// -----[ cbgp_jni_call_boolean ]------------------------------------
-extern int cbgp_jni_call_boolean(JNIEnv * env, jobject joObject,
-				 const char * pcMethod,
-				 const char * pcSignature, ...);
-// -----[ cbgp_jni_call_Object ]---------------------------------------
-extern jobject cbgp_jni_call_Object(JNIEnv * env, jobject joObject,
-				    const char * pcMethod,
-				    const char * pcSignature, ...);
+  ///////////////////////////////////////////////////////////////////
+  // Helper functions for Java objects creation and method calls
+  ///////////////////////////////////////////////////////////////////
 
-
-/////////////////////////////////////////////////////////////////////
-// Management of standard Java API objects
-/////////////////////////////////////////////////////////////////////
-
-// -----[ cbgp_jni_new_Short ]---------------------------------------
-extern jobject cbgp_jni_new_Short(JNIEnv * jEnv, jshort jsValue);
-// -----[ cbgp_jni_new_String ]--------------------------------------
-extern jstring cbgp_jni_new_String(JNIEnv * jEnv, const char * pcString);
-// -----[ cbgp_jni_new_Vector ]--------------------------------------
-extern jobject cbgp_jni_new_Vector(JNIEnv * jEnv);
-// -----[ cbgp_jni_Vector_add ]--------------------------------------
-extern int cbgp_jni_Vector_add(JNIEnv * jEnv, jobject joVector,
-			       jobject joObject);
-/*
-// -----[ cbgp_jni_new_HashMap ]-------------------------------------
-extern jobject cbgp_jni_new_HashMap(JNIEnv * jEnv);
-// -----[ cbgp_jni_HashMap_put ]-------------------------------------
-extern jobject cbgp_jni_HashMap_put(JNIEnv * jEnv, jobject joHashMap,
-				    jobject joKey, jobject joValue);
-*/
-// -----[ cbgp_jni_new_Hashtable ]-----------------------------------
-extern jobject cbgp_jni_new_Hashtable(JNIEnv * jEnv);
-// -----[ cbgp_jni_Hashtable_put ]-----------------------------------
-extern jobject cbgp_jni_Hashtable_put(JNIEnv * jEnv, jobject joHashMap,
-				      jobject joKey, jobject joValue);
-// -----[ cbgp_jni_new_ArrayList ]-----------------------------------
-extern jobject cbgp_jni_new_ArrayList(JNIEnv * jEnv);
-// -----[ cbgp_jni_ArrayList_add ]-----------------------------------
-extern int cbgp_jni_ArrayList_add(JNIEnv * jEnv, jobject joArrayList,
-				  jobject joItem);
-
-// -----[ jni_Object_hashCode ]--------------------------------------
-extern jint jni_Object_hashCode(JNIEnv * jEnv, jobject joObject);
+  // -----[ cbgp_jni_new ]-------------------------------------------
+  jobject cbgp_jni_new(JNIEnv * env, const char * pcClass,
+		       const char * pcConstr, ...);
+  // -----[ cbgp_jni_call_void ]-------------------------------------
+  int cbgp_jni_call_void(JNIEnv * env, jobject joObject,
+			 const char * pcMethod,
+			 const char * pcSignature, ...);
+  // -----[ cbgp_jni_call_boolean ]----------------------------------
+  int cbgp_jni_call_boolean(JNIEnv * env, jobject joObject,
+			    const char * pcMethod,
+			    const char * pcSignature, ...);
+  // -----[ cbgp_jni_call_Object ]-----------------------------------
+  jobject cbgp_jni_call_Object(JNIEnv * env, jobject joObject,
+			       const char * pcMethod,
+			       const char * pcSignature, ...);
+  
+  
+  ///////////////////////////////////////////////////////////////////
+  // Management of standard Java API objects
+  ///////////////////////////////////////////////////////////////////
+  
+  // -----[ cbgp_jni_new_Short ]-------------------------------------
+  jobject cbgp_jni_new_Short(JNIEnv * jEnv, jshort jsValue);
+  // -----[ cbgp_jni_new_String ]------------------------------------
+  jstring cbgp_jni_new_String(JNIEnv * jEnv, const char * pcString);
+  // -----[ cbgp_jni_new_Vector ]------------------------------------
+  jobject cbgp_jni_new_Vector(JNIEnv * jEnv);
+  // -----[ cbgp_jni_Vector_add ]------------------------------------
+  int cbgp_jni_Vector_add(JNIEnv * jEnv, jobject joVector,
+			  jobject joObject);
+  /*
+  // -----[ cbgp_jni_new_HashMap ]-----------------------------------
+  jobject cbgp_jni_new_HashMap(JNIEnv * jEnv);
+  // -----[ cbgp_jni_HashMap_put ]-----------------------------------
+  jobject cbgp_jni_HashMap_put(JNIEnv * jEnv, jobject joHashMap,
+  jobject joKey, jobject joValue);
+  */
+  // -----[ cbgp_jni_new_Hashtable ]---------------------------------
+  jobject cbgp_jni_new_Hashtable(JNIEnv * jEnv);
+  // -----[ cbgp_jni_Hashtable_put ]---------------------------------
+  jobject cbgp_jni_Hashtable_put(JNIEnv * jEnv, jobject joHashMap,
+				 jobject joKey, jobject joValue);
+  // -----[ cbgp_jni_new_ArrayList ]---------------------------------
+  jobject cbgp_jni_new_ArrayList(JNIEnv * jEnv);
+  // -----[ cbgp_jni_ArrayList_add ]---------------------------------
+  int cbgp_jni_ArrayList_add(JNIEnv * jEnv, jobject joArrayList,
+			     jobject joItem);
+  
+  // -----[ jni_Object_hashCode ]------------------------------------
+  jint jni_Object_hashCode(JNIEnv * jEnv, jobject joObject);
+  
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __JNI_BASE_H__ */
