83b3cfe62e636c922d0c2466065ccc3959d76d44
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-07-27 08:01:30 +0000

    Changes: - modified to return explicitly set hashcode (Java's hashcode does not work)

--- src/jni/be/ac/ucl/ingi/cbgp/ProxyObject.java
+++ src/jni/be/ac/ucl/ingi/cbgp/ProxyObject.java
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 27/02/2006
-// @lastdate 06/10/2006
+// @lastdate 04/06/2007
 // ==================================================================
 
 package be.ac.ucl.ingi.cbgp; 
@@ -12,27 +12,39 @@ package be.ac.ucl.ingi.cbgp;
 public class ProxyObject extends Object
 {
 
-	protected CBGP cbgp;
+    protected CBGP cbgp;
+    protected int hash_code;
 	
-	// -----[ ProxyObject ]-----------------------------------------
-	protected ProxyObject(CBGP cbgp)
-	{
-		this.cbgp= cbgp;
-	}
+    // -----[ ProxyObject ]-----------------------------------------
+    protected ProxyObject(CBGP cbgp)
+    {
+	this.cbgp= cbgp;
+    }
 	
-	// -----[ getCBGP ]---------------------------------------------
-	public CBGP getCBGP()
-	{
-		return cbgp;
-	}
+    // -----[ getCBGP ]---------------------------------------------
+    public CBGP getCBGP()
+    {
+	return cbgp;
+    }
 	
-	// -----[ finalize ]--------------------------------------------
-	protected void finalize()
-	{
-		_jni_unregister();
-	}
+    // -----[ finalize ]--------------------------------------------
+    protected void finalize()
+    {
+	_jni_unregister();
+    }
 	
-	// -----[ _jni_unregister ]-----------------------------------------
-	private native void _jni_unregister();
+    // -----[ _jni_unregister ]-----------------------------------------
+    private native void _jni_unregister();
+
+    // -----[ hashCode ]------------------------------------------------
+    /**
+     * This is a replacement for Object's hashCode() method that ensures
+     * the returned hashcode is unique. We base it on the memory address
+     * of the proxied C object.
+     */ 
+    public int hashCode()
+    {
+	return hash_code;
+    }
 	
 }
