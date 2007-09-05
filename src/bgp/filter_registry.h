8d9333cf0d12559440c9d7411643f135253fcc3e
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-09-05 14:06:51 +0000

    Fixed: - prepared to be included in C++ code

--- src/bgp/filter_registry.h
+++ src/bgp/filter_registry.h
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 01/03/2004
-// @lastdate 17/05/2005
+// @lastdate 28/08/2007
 // ==================================================================
 
 #ifndef __FILTER_REGISTRY_H__
@@ -17,21 +17,24 @@
 #define FILTER_PARSER_ERROR_UNEXPECTED -1
 #define FILTER_PARSER_ERROR_END        -2
 
-// ----- ft_registry_predicate_get ----------------------------------
-//extern SFtRegPredicate * ft_registry_predicate_get(uint8_t uID);
-// ----- ft_registry_action_get -------------------------------------
-//extern SFtRegAction * ft_registry_action_get(uint8_t uID);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-// ----- ft_registry_predicate_parse ----------------------------
-extern int ft_registry_predicate_parse(char * pcExpr,
-				       SFilterMatcher ** ppMatcher);
-// ----- ft_registry_action_parse -----------------------------------
-extern int ft_registry_action_parse(char * pcExpr,
-				    SFilterAction ** ppAction);
+  // -----[ ft_registry_predicate_parser ]---------------------------
+  int ft_registry_predicate_parser(char * pcExpr,
+				   SFilterMatcher ** ppMatcher);
+  // -----[ ft_registry_action_parser ]------------------------------
+  int ft_registry_action_parser(char * pcExpr,
+				SFilterAction ** ppAction);
 
-// ----- _ft_registry_init --------------------------------------
-extern void _ft_registry_init();
-// ----- _ft_registry_destroy -----------------------------------
-extern void _ft_registry_destroy();
+  // -----[ _ft_registry_init ]--------------------------------------
+  void _ft_registry_init();
+  // -----[ _ft_registry_destroy ]-----------------------------------
+  void _ft_registry_destroy();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
