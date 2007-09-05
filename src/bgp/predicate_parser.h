2042f120a847c846c91184eb4f1419dc3ff18750
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-09-05 13:59:48 +0000

    New: - predicate parser has been completely rewritten.

--- src/bgp/predicate_parser.h
+++ src/bgp/predicate_parser.h
@@ -3,18 +3,37 @@
 //
 // @author Bruno Quoitin (bqu@info.ucl.ac.be)
 // @date 29/02/2004
-// @lastdate 01/03/2004
+// @lastdate 05/09/2007
 // ==================================================================
 
 #ifndef __PREDICATE_PARSER_H__
 #define __PREDICATE_PARSER_H__
 
+#include <libgds/log.h>
 #include <bgp/filter.h>
 
-// ----- predicate_parse --------------------------------------------
-extern int predicate_parse(char ** ppcExpr,
-			   SFilterMatcher ** ppMatcher);
+#define PREDICATE_PARSER_END                     1
+#define PREDICATE_PARSER_SUCCESS                 0
+#define PREDICATE_PARSER_ERROR_UNEXPECTED        -1
+#define PREDICATE_PARSER_ERROR_UNFINISHED_STRING -2
+#define PREDICATE_PARSER_ERROR_ATOM              -3
+#define PREDICATE_PARSER_ERROR_UNEXPECTED_EOF    -4
+#define PREDICATE_PARSER_ERROR_LEFT_EXPR         -5
+#define PREDICATE_PARSER_ERROR_PAR_MISMATCH      -6
+#define PREDICATE_PARSER_ERROR_STATE_TOO_LARGE   -7
+#define PREDICATE_PARSER_ERROR_UNARY_OP          -8
 
-extern void predicate_parser_test();
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+  // -----[ predicate_parser ]---------------------------------------
+  int predicate_parser(char * pcExpr, SFilterMatcher ** ppMatcher);
+  // -----[ predicate_parser_perror ]--------------------------------
+  void predicate_parser_perror(SLogStream * pStream, int iErrorCode);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __PREDICATE_PARSER_H__ */
