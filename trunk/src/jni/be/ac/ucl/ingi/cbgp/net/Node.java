238059f549191674836cd7d46721798223c6a471
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-10-25 09:55:38 +0000

    Changes: - list of protocols is now obtained dynamically -> getProtocols() / hasProtocol() - new methods for adding LTL, PTP and PTMP links

--- trunk/src/jni/be/ac/ucl/ingi/cbgp/net/Node.java
+++ trunk/src/jni/be/ac/ucl/ingi/cbgp/net/Node.java
@@ -8,8 +8,6 @@
 
 package be.ac.ucl.ingi.cbgp.net; 
 
-import java.util.Enumeration;
-import java.util.Hashtable;
 import java.util.Vector;
 
 import be.ac.ucl.ingi.cbgp.CBGP;
@@ -26,22 +24,22 @@ public class Node extends ProxyObject
 {
 
     // -----[ public constants ]-------------------------------------
-    public static final String PROTOCOL_BGP= "bgp";
-    public static final String PROTOCOL_HLP= "hlp";
+	public static final String PROTOCOL_ICMP= "icmp";
+	public static final String PROTOCOL_IPIP= "ipip";
+    public static final String PROTOCOL_BGP = "bgp";
+    public static final String PROTOCOL_HLP = "hlp";
 
     // -----[ protected attributes ]---------------------------------
     protected IPAddress address;
-    protected Hashtable protocols;
 
     // -----[ Node ]-------------------------------------------------
     /**
      * Node's constructor.
      */
-    public Node(CBGP cbgp, IPAddress address, Hashtable protocols)
+    public Node(CBGP cbgp, IPAddress address)
     {
     	super(cbgp);
     	this.address= address;
-    	this.protocols= protocols;
     }
 
     // -----[ getAddress ]-------------------------------------------
@@ -50,24 +48,36 @@ public class Node extends ProxyObject
      */
     public IPAddress getAddress()
     {
-	return address;
+    	return address;
     }
 
     // -----[ getLatitude ]------------------------------------------
+    /**
+     * Get the latitude of this node.
+     */
     public native synchronized float getLatitude()
-	throws CBGPException;
+		throws CBGPException;
 
     // -----[ setLatitude ]------------------------------------------
+    /**
+     * Set the latitude of this node.
+     */
     public native synchronized void setLatitude(float latitude)
-	throws CBGPException;
+		throws CBGPException;
 
     // -----[ getLongitude ]-----------------------------------------
+    /**
+     * Get the longitude of this node.
+     */
     public native synchronized float getLongitude()
-	throws CBGPException;
+		throws CBGPException;
 
     // -----[ setLongitude ]-----------------------------------------
+    /**
+     * Set the longitude of this node.
+     */
     public native synchronized void setLongitude(float longitude)
-	throws CBGPException;
+		throws CBGPException;
 
     // -----[ getName ]----------------------------------------------
     /**
@@ -85,51 +95,94 @@ public class Node extends ProxyObject
 
     // -----[ hasProtocol ]------------------------------------------
     /**
-     * Returns 'true' if the given protocol is supported by this node.
+     * Returns 'true' if the given protocol is supported by this
+     * node. Use the protocol constants PROTOCOL_XXX defined in this
+     * class.
      */
-    public boolean hasProtocol(String protocol)
-    {
-	if (protocols != null) {
-	    return protocols.containsKey(protocol);
-	}
-	return false;
-    }
+    public native synchronized boolean hasProtocol(String protocol)
+    	throws CBGPException;
 
     // -----[ getProtocols ]-----------------------------------------
-    public Enumeration getProtocols()
-    {
-	if (protocols != null)
-	    return protocols.keys();
-	return null;
-    }
+    /**
+     * Returns the list of protocols supported by this node. The
+     * protocols are identified by the protocol constants
+     * PROTOCOL_XXX defined in this class.
+     */
+    public native synchronized Vector<String> getProtocols()
+    	throws CBGPException;
 
     // -----[ getBGP ]-----------------------------------------------
+    /**
+     * Returns a reference to the BGP router running on this node
+     * (if BGP is supported).
+     */
     public native synchronized be.ac.ucl.ingi.cbgp.bgp.Router getBGP()
-	throws CBGPException;
+		throws CBGPException;
     
     // -----[ recordRoute ]------------------------------------------
+    /**
+     * Records the IP-level route from this node towards the given
+     * destination address.
+     */
     public native synchronized IPTrace recordRoute(String sDstAddr)
 		throws CBGPException;
 
     // -----[ traceRoute ]-------------------------------------------
+    /**
+     * Traces the IP-level route from this node towards the given
+     * destination address.
+     */
     public native synchronized IPTrace traceRoute(String sDstAddr)
 		throws CBGPException;
     
     // -----[ getAddresses ]-----------------------------------------
+    /**
+     * Returns the list of addresses supported by this node.
+     */
     public native synchronized Vector getAddresses()
     	throws CBGPException;
     
+    // -----[ addLTLLink ]------------------------------------------
+    /**
+     * Adds a loopback-to-loopback (LTL) link to this node.
+     */
+    public native synchronized Link addLTLLink(Node dst, boolean bidir)
+    	throws CBGPException;
+    
+    // -----[ addPTPLink ]------------------------------------------
+    /**
+     * Adds a point-to-point (PTP) link to this node.
+     */
+    public native synchronized Link addPTPLink(Node dst, String siface, 
+    		String diface, boolean bidir)
+    	throws CBGPException;
+    
+    // -----[ addPTMPLink ]-----------------------------------------
+    /**
+     * Adds a point-to-multipoint (PTMP) link to this node. The
+     * destination is a Subnet object.
+     */
+    public native synchronized Link addPTMPLink(Subnet subnet, String siface)
+    	throws CBGPException; 
+
     // -----[ getLinks ]---------------------------------------------
+    /**
+     * Returns the list of links departing from this node.
+     */
     public native synchronized Vector getLinks()
 		throws CBGPException;
     
     // -----[ getRT ]------------------------------------------------
+    /**
+     * Returns the list of routes installed in the routing table
+     * of this node.
+     */
     public native synchronized Vector getRT(String sPrefix)
 		throws CBGPException;
 
     // -----[ addRoute ]---------------------------------------------
     /**
-     * Add a static route to a node.
+     * Add a static route to this node.
      *
      * @param sPrefix the route destination prefix
      * @param sNextHop the gateway
@@ -137,7 +190,7 @@ public class Node extends ProxyObject
      */
     public native synchronized
 	void addRoute(String sPrefix, String sNexthop, 
-		      int iWeight)
+			int iWeight)
 	    throws CBGPException;
 
     // -----[ toString ]---------------------------------------------
