d0ef0e3b22836127491213a7ef7b1ecabd153773
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-10-25 10:19:19 +0000

    Changes: - new files to implement JNI of subnets

---
+++ trunk/src/jni/impl/net_Subnet.h
@@ -0,0 +1,27 @@
+// ==================================================================
+// @(#)net_Subnet.h
+//
+// @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
+// @date 19/10/2007
+// @lastdate 19/10/2007
+// ==================================================================
+
+#ifndef __JNI_NET_SUBNET_H__
+#define __JNI_NET_SUBNET_H__
+
+#include <jni_md.h>
+#include <jni.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  // -----[ cbgp_jni_new_net_Subnet ]----------------------------------
+  jobject cbgp_jni_new_net_Subnet(JNIEnv * jEnv, jobject joCBGP,
+				  SNetSubnet * pSubnet);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __JNI_NET_SUBNET_H__ */
