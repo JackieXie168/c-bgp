217784e3cd73723d342a87dbf4adeb501ec83016
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-11-21 10:42:05 +0000

    Changes: - constructor updated (need gateway information)

--- src/jni/be/ac/ucl/ingi/cbgp/bgp/Route.java
+++ src/jni/be/ac/ucl/ingi/cbgp/bgp/Route.java
@@ -34,136 +34,127 @@ public class Route extends be.ac.ucl.ingi.cbgp.Route
     // -----[ BGPRoute ]---------------------------------------------
     /**
      * BGPRoute's constructor. Builds a BGP route with its
-     * attributes. Currently only supports destination prefix and
-     * next-hop.
+     * attributes. Currently only supports destination prefix,
+     * next-hop, AS-Path, origin, local-pref, MED and communities.
      */
-    public Route(IPPrefix prefix, IPAddress nexthop, long lLocalPref, long lMED,
+    public Route(IPPrefix prefix, IPAddress nexthop, IPAddress gateway,
+    		long lLocalPref, long lMED,
 		    boolean bBest, boolean bFeasible, byte bOrigin,
 		    ASPath path, boolean bInternal,
-		    Communities communities)
-    {
-	super(prefix, nexthop, bBest, bFeasible);
+		    Communities communities) {
+    	super(prefix, nexthop, gateway, bBest, bFeasible);
 
-	// Attributes
-	this.lLocalPref= lLocalPref;
-	this.lMED= lMED;
-	this.bOrigin= bOrigin;
-	this.path= path;
-	this.bInternal= bInternal;
-	this.communities= communities;
+    	// Attributes
+    	this.lLocalPref= lLocalPref;
+    	this.lMED= lMED;
+    	this.bOrigin= bOrigin;
+    	this.path= path;
+    	this.bInternal= bInternal;
+    	this.communities= communities;
     }
 
     // -----[ getLocalPref ]-----------------------------------------
     /**
      * Returns the route's local-preference.
      */
-    public long getLocalPref()
-    {
-	return lLocalPref;
+    public long getLocalPref() {
+    	return lLocalPref;
     }
 
     // -----[ getMED ]-----------------------------------------------
     /**
      * Returns the routes's MED.
      */
-    public long getMED()
-    {
-	return lMED;
+    public long getMED() {
+    	return lMED;
     }
 
     // -----[ getOrigin ]--------------------------------------------
     /**
      * Returns the route's origin.
      */
-    public byte getOrigin()
-    {
-	return bOrigin;
+    public byte getOrigin() {
+    	return bOrigin;
     }
 
     // -----[ getPath ]----------------------------------------------
     /**
      * Returns the route's AS-Path.
      */
-    public ASPath getPath()
-    {
-	return path;
+    public ASPath getPath() {
+    	return path;
     }
 
     // -----[ getCommunities ]---------------------------------------
     /**
      * Returns the route's Communities.
      */
-    public Communities getCommunities()
-    {
-	return communities;
+    public Communities getCommunities() {
+    	return communities;
     }
 
     // -----[ isInternal ]-------------------------------------------
     /**
      * Tests if the route is internal (originated on this router).
      */
-    public boolean isInternal()
-    {
-	return bInternal;
+    public boolean isInternal() {
+    	return bInternal;
     }
 
     // -----[ originToString ]---------------------------------------
     /**
      * Converts the given origin to a String.
      */
-    public static String originToString(byte bOrigin)
-    {
-	switch (bOrigin) {
-	case ROUTE_ORIGIN_IGP: return "IGP";
-	case ROUTE_ORIGIN_EGP: return "EGP";
-	case ROUTE_ORIGIN_INCOMPLETE: return "INCOMPLETE";
-	default:
-	    return "DEFAULT";
-	}
+    public static String originToString(byte bOrigin) {
+    	switch (bOrigin) {
+    	case ROUTE_ORIGIN_IGP       : return "IGP";
+    	case ROUTE_ORIGIN_EGP       : return "EGP";
+    	case ROUTE_ORIGIN_INCOMPLETE: return "INCOMPLETE";
+    	default:
+    		return "DEFAULT";
+    	}
     }
 
     // -----[ toString ]---------------------------------------------
     /**
      * Converts this BGP route to a String.
      */
-    public String toString()
-    {
-	String s= "";
+    public String toString() {
+    	String s= "";
 
-	// Flags
-	if (bInternal)
-	    s+= "i";
-	else
-	    s+= (bFeasible?"*":" ");
-	s+= (bBest?">":" ");
-	s+= " ";
+    	// Flags
+    	if (bInternal)
+    		s+= "i";
+    	else
+    		s+= (bFeasible?"*":" ");
+    	s+= (bBest?">":" ");
+    	s+= " ";
 
-	// Attributes
-	s+= prefix;
-	s+= "\t";
-	s+= nexthop;
-	s+= "\t";
-	s+= lLocalPref;
-	s+= "\t";
-	s+= lMED;
-	s+= "\t";
-	if (path != null) {
-	    s+= path;
-	}
-	s+= "\t";
-	switch (bOrigin) {
-	case ROUTE_ORIGIN_IGP: s+= "i"; break;
-	case ROUTE_ORIGIN_EGP: s+= "e"; break;
-	case ROUTE_ORIGIN_INCOMPLETE: s+= "?"; break;
-	default:
-	    s+= "?";
-	}
-	if (communities != null) {
-	    s+= "\t";
-	    s+= communities;
-	}
+    	// Attributes
+    	s+= prefix;
+    	s+= "\t";
+    	s+= nexthop;
+    	s+= "\t";
+    	s+= lLocalPref;
+    	s+= "\t";
+    	s+= lMED;
+    	s+= "\t";
+    	if (path != null)
+    		s+= path;
+    	s+= "\t";
+    	switch (bOrigin) {
+    	case ROUTE_ORIGIN_IGP: s+= "i"; break;
+    	case ROUTE_ORIGIN_EGP: s+= "e"; break;
+    	case ROUTE_ORIGIN_INCOMPLETE: s+= "?"; break;
+    	default:
+    		s+= "?";
+    	}
+    	if (communities != null) {
+    		s+= "\t";
+    		s+= communities;
+    	}
 
-	return s;
+    	return s;
     }
 
 }
