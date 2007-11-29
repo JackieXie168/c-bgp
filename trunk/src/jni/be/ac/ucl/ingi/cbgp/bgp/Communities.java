7fc6167aa0659f1c2fbc2c8176cae384f7b70c60
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2007-11-29 12:03:30 +0000

    Changes: - Vector element is not typed.

--- trunk/src/jni/be/ac/ucl/ingi/cbgp/bgp/Communities.java
+++ trunk/src/jni/be/ac/ucl/ingi/cbgp/bgp/Communities.java
@@ -18,29 +18,26 @@ public class Communities
 {
     
     // -----[ protected attributes ]---------------------------------
-    protected Vector communities= null;
+    protected Vector<Integer> communities= null;
 
     // -----[ Communities ]------------------------------------------
     /**
      * Communities's constructor.
      */
-    public Communities()
-    {
-	this.communities= new Vector();
+    public Communities() {
+    	this.communities= new Vector<Integer>();
     }
 
     // -----[ append ]-----------------------------------------------
     /**
      * Append a new Community.
      */
-    public void append(int community)
-    {
+    public void append(int community) {
     	communities.add(new Integer(community));
     }
 
     // -----[ getCommunityCount ]-----------------------------------
-    public int getCommunityCount()
-    {
+    public int getCommunityCount() {
     	return communities.size();
     }
     
@@ -48,8 +45,7 @@ public class Communities
     /**
      * Return the Community at the given position.
      */
-    public int getCommunity(int iIndex)
-    {
+    public int getCommunity(int iIndex) {
     	return ((Integer) communities.get(iIndex)).intValue();
     }
     
@@ -57,33 +53,31 @@ public class Communities
     /**
      * Convert a community to its String representation.
      */
-    public static String communityToString(int community)
-    {
-	long lCommunity= (community >= 0)
-	    ?community
-	    :(0x100000000L +community);
-	String s= "";
-	s+= (lCommunity >> 16);
-	s+= ":";
-	s+= (lCommunity & 0xffff);
-	return s;
+    public static String communityToString(int community) {
+    	long lCommunity= (community >= 0)
+	    	?community
+	    			:(0x100000000L +community);
+    	String s= "";
+    	s+= (lCommunity >> 16);
+    	s+= ":";
+    	s+= (lCommunity & 0xffff);
+    	return s;
     }
 
     // -----[ toString ]---------------------------------------------
     /**
      * Convert the Communities to a String.
      */
-    public String toString()
-    {
-	String s= "";
+    public String toString() {
+    	String s= "";
 
-	for (int iIndex= communities.size(); iIndex > 0; iIndex--) {
-	    if (iIndex < communities.size()) {
-		s+= " ";
-	    }
-	    s+= communityToString(getCommunity(iIndex-1));
-	}
-	return s;
+    	for (int iIndex= communities.size(); iIndex > 0; iIndex--) {
+    		if (iIndex < communities.size()) {
+    			s+= " ";
+    		}
+    		s+= communityToString(getCommunity(iIndex-1));
+    	}
+    	return s;
     }
 
 }
