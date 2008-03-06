0967b939c0715d470dc779b279909ee2f22212a4
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2008-03-06 16:17:06 +0000

    Changes: - new method equals() - new method hashCode()

--- src/jni/be/ac/ucl/ingi/cbgp/net/Element.java
+++ src/jni/be/ac/ucl/ingi/cbgp/net/Element.java
@@ -5,6 +5,28 @@ import be.ac.ucl.ingi.cbgp.ProxyObject;
 
 public abstract class Element extends ProxyObject {
 	
-	protected Element(CBGP cbgp) { super(cbgp); }
+	// -----[ constructor ]-----------------------------------------
+	protected Element(CBGP cbgp) {
+		super(cbgp);
+	}
+	
+	// -----[ getId ]-----------------------------------------------
+	public abstract String getId();
+	
+	// -----[ equals ]----------------------------------------------
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (obj.getClass() != this.getClass())
+			return false;
+		return ((Element) obj).getId().equals(getId());
+	}
+	
+	// -----[ hashCode ]--------------------------------------------
+	@Override
+	public int hashCode() {
+		return getId().hashCode();
+	}
 
 }
