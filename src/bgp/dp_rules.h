51c430f10b60125fc65e6f917ba8d36f4a833d8b
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2008-04-07 09:20:14 +0000

    Changes: - decision process rules moved from as.c/.h - cosmetic changes (naming guidelines)

--- src/bgp/dp_rules.h
+++ src/bgp/dp_rules.h
@@ -1,9 +1,9 @@
 // ==================================================================
 // @(#)dp_rules.h
 //
-// @author Bruno Quoitin (bqu@info.ucl.ac.be)
+// @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 13/11/2002
-// @lastdate 15/11/2005
+// @lastdate 10/03/2008
 // ==================================================================
 
 #ifndef __BGP_DP_RULES_H__
@@ -25,7 +25,16 @@
  * Note: the int return value is not used yet. Decision process rules
  * must return 0 for now.
  */
-typedef int (*FDPRule)(SBGPRouter * pRouter, SRoutes * pRoutes);
+typedef int (*FDPRule)(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+
+struct dp_rule_t {
+  char  * pcName;
+  FDPRule fRule;
+};
+
+// -----[ decision process ]-----------------------------------------
+#define DP_NUM_RULES 11
+extern struct dp_rule_t DP_RULES[DP_NUM_RULES];
 
 // ----- FDPRouteCompare -----
 /**
@@ -37,40 +46,45 @@ typedef int (*FDPRule)(SBGPRouter * pRouter, SRoutes * pRoutes);
  * Note: function of this type MUST be deterministic, i.e. the
  * ordering they define must be complete
  */
-typedef int (*FDPRouteCompare)(SBGPRouter * pRouter,
-			       SRoute * pRoute1,
-			       SRoute * pRoute2);
+typedef int (*FDPRouteCompare)(bgp_router_t * pRouter,
+			       bgp_route_t * pRoute1,
+			       bgp_route_t * pRoute2);
 
-int dp_rule_no_selection(SBGPRouter * pRouter, SRoutes * pRoutes);
-int dp_rule_lowest_nh(SBGPRouter * pRouter, SRoutes * pRoutes);
-int dp_rule_lowest_path(SBGPRouter * pRouter, SRoutes * pRoutes);
-// ----- bgp_dp_rule_local ------------------------------------------
-extern void bgp_dp_rule_local(SBGPRouter * pRouter, SRoutes * pRoutes);
+#ifdef __cplusplus
+extern "C" {
+#endif 
 
-// ----- dp_rule_highest_pref ---------------------------------------
-extern int dp_rule_highest_pref(SBGPRouter * pRouter, SRoutes * pRoutes);
-// ----- dp_rule_shortest_path --------------------------------------
-extern int dp_rule_shortest_path(SBGPRouter * pRouter, SRoutes * pRoutes);
-// ----- dp_rule_lowest_origin --------------------------------------
-extern int dp_rule_lowest_origin(SBGPRouter * pRouter, SRoutes * pRoutes);
-// ----- dp_rule_lowest_med -----------------------------------------
-extern int dp_rule_lowest_med(SBGPRouter * pRouter, SRoutes * pRoutes);
-// ----- dp_rule_ebgp_over_ibgp -------------------------------------
-extern int dp_rule_ebgp_over_ibgp(SBGPRouter * pRouter, SPtrArray * pRoutes);
-// ----- dp_rule_nearest_next_hop -----------------------------------
-extern int dp_rule_nearest_next_hop(SBGPRouter * pRouter, SRoutes * pRoutes);
-// ----- dp_rule_lowest_router_id -----------------------------------
-extern int dp_rule_lowest_router_id(SBGPRouter * pRouter, SRoutes * pRoutes);
-// ----- dp_rule_shortest_cluster_list ------------------------------
-extern int dp_rule_shortest_cluster_list(SBGPRouter * pRouter,
-					 SRoutes * pRoutes);
-// ----- dp_rule_lowest_neighbor_address ----------------------------
-extern int dp_rule_lowest_neighbor_address(SBGPRouter * pRouter,
-					   SRoutes * pRoutes);
+  int dp_rule_no_selection(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  int dp_rule_lowest_nh(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  int dp_rule_lowest_path(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  // ----- bgp_dp_rule_local ----------------------------------------
+  void bgp_dp_rule_local(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  // ----- dp_rule_highest_pref -------------------------------------
+  int dp_rule_highest_pref(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  // ----- dp_rule_shortest_path ------------------------------------
+  int dp_rule_shortest_path(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  // ----- dp_rule_lowest_origin ------------------------------------
+  int dp_rule_lowest_origin(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  // ----- dp_rule_lowest_med ---------------------------------------
+  int dp_rule_lowest_med(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  // ----- dp_rule_ebgp_over_ibgp -----------------------------------
+  int dp_rule_ebgp_over_ibgp(bgp_router_t * pRouter, SPtrArray * pRoutes);
+  // ----- dp_rule_nearest_next_hop ---------------------------------
+  int dp_rule_nearest_next_hop(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  // ----- dp_rule_lowest_router_id ---------------------------------
+  int dp_rule_lowest_router_id(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  // ----- dp_rule_shortest_cluster_list ----------------------------
+  int dp_rule_shortest_cluster_list(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  // ----- dp_rule_lowest_neighbor_address --------------------------
+  int dp_rule_lowest_neighbor_address(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  
+  // ----- dp_rule_final ----------------------------------------------
+  int dp_rule_final(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
+  // ----- dp_rule_lowest_delay ---------------------------------------
+  int dp_rule_lowest_delay(bgp_router_t * pRouter, bgp_routes_t * pRoutes);
 
-// ----- dp_rule_final ----------------------------------------------
-extern int dp_rule_final(SBGPRouter * pRouter, SRoutes * pRoutes);
-// ----- dp_rule_lowest_delay ---------------------------------------
-extern int dp_rule_lowest_delay(SBGPRouter * pRouter, SRoutes * pRoutes);
-
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* __BGP_DP_RULES_H__ */
