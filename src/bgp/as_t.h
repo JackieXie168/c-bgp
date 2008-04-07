4a2a588550379a98e9f6161861b601283d4585af
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2008-04-07 09:25:12 +0000

    Changes: - cosmetic changes (naming guidelines)

--- src/bgp/as_t.h
+++ src/bgp/as_t.h
@@ -1,37 +1,39 @@
 // ==================================================================
 // @(#)as_t.h
 //
-// @author Bruno Quoitin (bqu@info.ucl.ac.be)
+// @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @author Sebastien Tandel (standel@info.ucl.ac.be)
 // @date 23/05/2003
-// @lastdate 10/04/2006
+// @lastdate 11/03/2008
 // ==================================================================
 
 #ifndef __AS_T_H__
 #define __AS_T_H__
 
 #include <libgds/array.h>
+#include <bgp/peer-list.h>
 #include <bgp/rib_t.h>
 #include <bgp/route_reflector.h>
 #include <bgp/tie_breaks.h>
 #include <net/network.h>
+#include <bgp/types.h>
 
 typedef struct {
-  uint16_t uNumber;
+  uint16_t uASN;
   char * pcName;
   SRadixTree * pRouters;
 } SBGPDomain;
 
 typedef struct TBGPRouter {
-  uint16_t uNumber;            // AS-number of the router
+  uint16_t uASN;            // AS-number of the router
   net_addr_t tRouterID;        // Router-ID of the router
-  SPtrArray * pPeers;          // List of neighbor routers
+  bgp_peers_t * pPeers;          // List of neighbor routers
   SRIB * pLocRIB;
-  SPtrArray * pLocalNetworks;  // List of locally originated prefixes
+  bgp_routes_t * pLocalNetworks;  // List of locally originated prefixes
   FTieBreakFunction fTieBreak;
   cluster_id_t tClusterID;     // Cluster-ID
   int iRouteReflector;
-  SNetNode * pNode;            // Reference to the node running this BGP router
+  net_node_t * pNode;            // Reference to the node running this BGP router
   SBGPDomain * pDomain;        // Reference to the AS containing this router
 
 #if defined __EXPERIMENTAL__ && defined __EXPERIMENTAL_WALTON__
@@ -42,5 +44,6 @@ typedef struct TBGPRouter {
 #endif
 
 } SBGPRouter;
+typedef SBGPRouter bgp_router_t;
 
 #endif
