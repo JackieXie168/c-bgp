7e5276e10b6a0630c3a5213b98ae0627c6f98068
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2008-04-07 09:42:04 +0000

    Changes: - cosmetic changes (naming guidelines) - re-organisation of handling of incoming IP-IP messages - re-organisation of sending of IP-IP messages (addr/oif options not supported anymore)

--- trunk/src/net/ipip.h
+++ trunk/src/net/ipip.h
@@ -1,9 +1,9 @@
 // ==================================================================
 // @(#)ipip.h
 //
-// @author Bruno Quoitin (bqu@info.ucl.ac.be)
+// @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 25/02/2004
-// @lastdate 16/04/2007
+// $Id: ipip.h,v 1.4 2008-04-07 09:42:04 bqu Exp $
 // ==================================================================
 
 #ifndef __NET_IPIP_H__
@@ -18,15 +18,15 @@ extern "C" {
 #endif
 
   // ----- ipip_send ------------------------------------------------
-  int ipip_send(SNetNode * pNode, net_addr_t tDstAddr,
-		SNetMessage * pMessage);
+  int ipip_send(net_node_t * node, net_addr_t dst_addr,
+		net_msg_t * msg);
   // ----- ipip_event_handler ---------------------------------------
-  int ipip_event_handler(SSimulator * pSimulator,
-			 void * pHandler, SNetMessage * pMessage);
+  int ipip_event_handler(simulator_t * sim,
+			 void * handler, net_msg_t * msg);
   // -----[ ipip_link_create ]---------------------------------------
-  int ipip_link_create(SNetNode * pNode, net_addr_t tDstPoint,
-		       net_addr_t tAddr, SNetLink * pOutIface,
-		       net_addr_t tSrcAddr, SNetLink ** ppLink);
+  int ipip_link_create(net_node_t * node, net_addr_t end_point,
+		       net_addr_t addr, net_iface_t * oif,
+		       net_addr_t src_addr, net_iface_t ** ppLink);
 
 #ifdef __cplusplus
 }
