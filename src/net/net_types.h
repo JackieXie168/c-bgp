a02ff162fe5c75373256d111980f920ae2c087bd
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2008-04-07 09:04:42 +0000

    Changes: - cosmetic changes (naming guidelines) - SNetNode -> net_node_t - SNetSubnet -> net_subnet_t - SNetIface -> net_iface_t - SNetwork -> network_t - ...

--- src/net/net_types.h
+++ src/net/net_types.h
@@ -4,7 +4,7 @@
 // @author Stefano Iasi (stefanoia@tin.it)
 // @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 30/06/2005
-// @lastdate 15/02/2008
+// @lastdate 11/03/2008
 // ==================================================================
 
 #ifndef __NET_TYPES_H__
@@ -12,6 +12,7 @@
 
 #include <libgds/array.h>
 #include <libgds/radix-tree.h>
+
 #include <net/link_attr.h>
 #include <net/prefix.h>
 #include <net/routing_t.h>
@@ -24,8 +25,8 @@ typedef uint8_t   ospf_path_type_t;
 typedef SPtrArray next_hops_list_t;
 typedef SPtrArray SOSPFRouteInfoList;
 
-typedef SPtrArray SNetLinks;
-typedef SPtrArray SNetSubnets;
+typedef SPtrArray net_ifaces_t;
+typedef SPtrArray net_subnets_t;
 
 typedef uint32_t net_link_delay_t;
 #define NET_LINK_DELAY_INFINITE UINT32_MAX
@@ -33,6 +34,10 @@ typedef uint32_t net_link_load_t;
 #define NET_LINK_MAX_CAPACITY UINT32_MAX
 #define NET_LINK_MAX_LOAD UINT32_MAX
 
+typedef struct {
+  float latitude;    
+  float longitude;
+} coord_t;
 
 //////////////////////////////////////////////////////////////////////
 //
@@ -43,38 +48,48 @@ typedef uint32_t net_link_load_t;
 // ----- FPayLoadDestroy -----
 typedef void (*FPayLoadDestroy)(void ** pPayLoad);
 
-// ----- SNetMessage -----
 typedef struct {
-  net_addr_t tSrcAddr;
-  net_addr_t tDstAddr;
-  uint8_t uProtocol;
-  uint8_t uTTL;
-  net_tos_t tTOS;
-  void * pPayLoad;
-  FPayLoadDestroy fDestroy;
-} SNetMessage;
+  FPayLoadDestroy destroy;
+} net_msg_ops_t;
 
-// ----- FNetNodeHandleEvent -----
-typedef int (*FNetNodeHandleEvent)(SSimulator * pSimulator,
-				   void * pHandler,
-				   SNetMessage * pMessage);
-// ----- FNetNodeHandlerDestroy -----
-typedef void (*FNetNodeHandlerDestroy)(void ** ppHandler);
-
-// ----- SNetProtocol -----
+// -----[ net_msg_t ]-----
 typedef struct {
-  void * pHandler;
-  FNetNodeHandleEvent fHandleEvent;
-  FNetNodeHandlerDestroy fDestroy;
-} SNetProtocol;
+  net_addr_t      src_addr;
+  net_addr_t      dst_addr;
+  uint8_t         protocol;
+  uint8_t         ttl;
+  net_tos_t       tos;
+  void          * payload;
+  net_msg_ops_t   ops;
+} net_msg_t;
 
-// Maximum number of supported protocols
-#define NET_PROTOCOL_MAX  3
+// -----[ FNetProtoHandleEvent ]-----
+typedef int (*FNetProtoHandleEvent)(simulator_t * sim,
+				    void * handler,
+				    net_msg_t * msg);
+// ----- FNetProtoHandlerDestroy -----
+typedef void (*FNetProtoHandlerDestroy)(void ** handler_ref);
 
-// ----- SNetProtocols -----
+// Protocol numbers
+typedef enum {
+  NET_PROTOCOL_ICMP,
+  NET_PROTOCOL_BGP,
+  NET_PROTOCOL_IPIP,
+  NET_PROTOCOL_MAX,
+} net_protocol_id_t;
+
+// -----[ net_protocol_t ]-----
 typedef struct {
-  SNetProtocol * data[NET_PROTOCOL_MAX];
-} SNetProtocols;
+  void                    * pHandler;
+  FNetProtoHandleEvent      fHandleEvent;
+  FNetProtoHandlerDestroy   fDestroy;
+} net_protocol_t;
+
+// -----[ net_protocols_t ]-----
+typedef struct {
+  net_protocol_t * data[NET_PROTOCOL_MAX];
+} net_protocols_t;
+
 
 //////////////////////////////////////////////////////////////////////
 //
@@ -83,10 +98,11 @@ typedef struct {
 //////////////////////////////////////////////////////////////////////
 
 typedef struct {
-  STrie * pNodes;            // Used to index all nodes (key=IP address)
-  SNetSubnets  * pSubnets;   // Used to index all subnets (key=IP prefix)
-  SSimulator * pSimulator;
-} SNetwork;
+  STrie         * nodes;      /* Global node index (key=IP address) */
+  net_subnets_t * subnets;    /* Global subnet index (key=IP prefix) */
+  simulator_t   * sim;
+} network_t;
+typedef network_t SNetwork;
 
 
 //////////////////////////////////////////////////////////////////////
@@ -99,12 +115,13 @@ typedef struct {
 typedef enum { DOMAIN_IGP, DOMAIN_OSPF } EDomainType;
 
 typedef struct {
-  uint16_t uNumber;
-  char * pcName;
-  STrie * pRouters;
-  EDomainType tType;
-  uint8_t uECMP;
-} SIGPDomain;
+  uint16_t      uNumber;
+  char        * pcName;
+  STrie       * pRouters;
+  EDomainType   tType;
+  uint8_t       uECMP;
+} igp_domain_t;
+typedef igp_domain_t SIGPDomain;
 
 
 //////////////////////////////////////////////////////////////////////
@@ -125,13 +142,13 @@ typedef struct {
 #define ospf_ri_get_NextHops(R) ((R)->aNextHops)
 
 typedef struct {
-  ospf_dest_type_t    tOSPFDestinationType;
-  SPrefix             sPrefix;
-  net_link_delay_t    uWeight;
-  ospf_area_t         tOSPFArea;
-  ospf_path_type_t    tOSPFPathType;
-  next_hops_list_t *  aNextHops;
-  net_route_type_t    tType;
+  ospf_dest_type_t   tOSPFDestinationType;
+  SPrefix            sPrefix;
+  net_link_delay_t   uWeight;
+  ospf_area_t        tOSPFArea;
+  ospf_path_type_t   tOSPFPathType;
+  next_hops_list_t * aNextHops;
+  net_route_type_t   tType;
 } SOSPFRouteInfo;
 
 typedef STrie SOSPFRT;
@@ -143,27 +160,28 @@ typedef STrie SOSPFRT;
 //
 //////////////////////////////////////////////////////////////////////
 
-// ----- SNetNode ---------------------------------------------------
+// ----- net_node_t ---------------------------------------------------
 /**
  * This type defines a node object.
  */
 typedef struct {
-  net_addr_t tAddr;
-  char * pcName;
-  SNetwork * pNetwork;
-  SNetLinks * pLinks;
-  SNetRT * pRT;
-#ifdef OSPF_SUPPORT
-  SUInt32Array  * pOSPFAreas; 
-  SOSPFRT       * pOspfRT;     // OSPF routing table
+  net_addr_t        tAddr;
+  char            * name;
+  network_t       * network;
+  net_ifaces_t    * ifaces;
+  net_rt_t        * rt;
+#ifdef OSPF_SUPPORT              /* TODO: should be moved to another
+				  * data structure */
+  SUInt32Array    * pOSPFAreas; 
+  SOSPFRT         * pOspfRT;     /* OSPF routing table */
 //  net_addr_t      ospf_id;
 #endif
-  SUInt16Array  * pIGPDomains; // TODO define type for list of domains numbers
-  SNetProtocols * pProtocols;  // Supported protocol handlers
-  SIGPDomain    * pDomain;
-  float           fLatitude;   // Geographical coordinates
-  float           fLongitude;
-} SNetNode;
+  SUInt16Array    * pIGPDomains; /* TODO: define type for list of domains */
+  net_protocols_t * protocols;   /* Supported protocol handlers */
+  igp_domain_t    * domain;
+  coord_t           coord;       /* Geographical coordinates */
+  int               syslog_enabled;
+} net_node_t;
 
 
 //////////////////////////////////////////////////////////////////////
@@ -172,19 +190,19 @@ typedef struct {
 //
 //////////////////////////////////////////////////////////////////////
 
-// ----- SNetSubnet -------------------------------------------------
+// -----[ net_subnet_t ]---------------------------------------------
 /**
  * Describe transit or stub networks: if there is only one router this
  * object describe a stub network. A transit network otherwise.
  */
 typedef struct {
-  SPrefix     sPrefix;
-  uint8_t     uType;
-  SNetLinks * pLinks;    /* links towards routers */
+  SPrefix        sPrefix;
+  uint8_t        uType;
+  net_ifaces_t * ifaces;    /* links towards routers */
 #ifdef OSPF_SUPPORT
-  ospf_area_t uOSPFArea; /* a subnetwork belongs to a single area */
+  ospf_area_t    uOSPFArea; /* a subnetwork belongs to a single area */
 #endif
-} SNetSubnet;
+} net_subnet_t;
 
 
 //////////////////////////////////////////////////////////////////////
@@ -200,7 +218,7 @@ typedef enum {
   NET_IFACE_PTP,
   NET_IFACE_PTMP,
   NET_IFACE_VIRTUAL,
-  NET_IFACE_UNKNOWN
+  NET_IFACE_MAX,
 } net_iface_type_t;
 
 // -----[ FNetIfaceSend ]--------------------------------------------
@@ -217,15 +235,14 @@ typedef enum {
  *   pContext : the link context
  *   ppDstIface: the destination's interface
  */
-struct TNetIface;
-typedef int (*FNetIfaceSend)(net_addr_t tPhysAddr,
-			     void * pContext,
-			     struct TNetIface ** pDstIface,
-			     SNetMessage ** ppMsg);
+struct net_iface_t;
+typedef int (*FNetIfaceSend)(struct net_iface_t * self,
+			     net_addr_t l2_addr,
+			     net_msg_t * msg);
 
 // -----[ FNetIfaceRecv ]--------------------------------------------
-typedef int (*FNetIfaceRecv)(struct TNetIface * pIface,
-			     SNetMessage * pMsg);
+typedef int (*FNetIfaceRecv)(struct net_iface_t * self,
+			     net_msg_t * msg);
 
 // -----[ FNetLinkDestroy ]------------------------------------------
 /**
@@ -241,23 +258,36 @@ typedef void (*FNetLinkDestroy)(void * pContext);
  * case, the destination is a pointer to the subnet.
  */
 typedef union {
-  struct TNetIface * pIface; // rtr, ptp links
-  SNetSubnet * pSubnet;     // ptmp links
-  net_addr_t tEndPoint;     // tunnels
+  struct net_iface_t * iface;     /* rtr, ptp links */
+  net_subnet_t       * subnet;    /* ptmp links */
+  net_addr_t           end_point; /* tunnel endpoint */
 } net_iface_dst_t;
 
-// -----[ SNetIface ]------------------------------------------------
+// -----[ Network interface operations ]-----------------------------
+typedef struct {
+  FNetIfaceSend   send;    /* Send message function */
+  FNetIfaceRecv   recv;    /* Recv message function */
+  FNetLinkDestroy destroy; /* Message destroy function */
+} net_iface_ops_t;
+
+typedef struct {
+  net_link_delay_t delay;    /* Propagation delay */
+  net_link_load_t  capacity; /* Link capacity */
+  net_link_load_t  load;     /* Link load */
+} net_iface_phys_t;
+
+// -----[ net_iface_t ]----------------------------------------------
 /**
  * This type defines a link object. A link is attached to a
  * source node. On this node, the link is identified by the
  * interface IP address (tIfaceAddr) and by the length of the
  * mask.
  */
-typedef struct TNetIface {
-  net_iface_type_t   tType;      // Type of interface
+typedef struct net_iface_t {
+  net_iface_type_t   type;       // Type of interface
   
   // --- Identification of source ---
-  SNetNode         * pSrcNode;   // Node to which the link is attached
+  net_node_t       * src_node;   // Node to which the link is attached
   net_addr_t         tIfaceAddr; // Local interface address:
                                  //   ptp: remote destination addr
                                  //   ptmp: local interface addr
@@ -265,31 +295,25 @@ typedef struct TNetIface {
   net_mask_t         tIfaceMask; // Local interface mask
                                  //   ptp,tun: 32
                                  //   ptmp: <32
-  uint8_t            uConnected; // Tells if the interface is connected
+  uint8_t            connected;  // Tells if the interface is connected
 
   // --- Identification of destination ---
-  net_iface_dst_t    tDest;      // Depends on tType
+  net_iface_dst_t    dest;       // Depends on tType
 
   // --- Physical attributes ---
-  net_link_delay_t   tDelay;     // Propagation delay
-  net_link_load_t    tCapacity;  // Link capacity
-  net_link_load_t    tLoad;      // Link load
-  uint8_t            uFlags;     // Flags: state (up/down), IGP_ADV
+  net_iface_phys_t   phys;
+  uint8_t            flags;      // Flags: state (up/down), IGP_ADV
 
   // --- IGP protocol attributes ---
   SNetIGPWeights   * pWeights;   // List of IGP weights (1/topo)
 
-  // --- Context and methods ---
-  void             * pContext;
-  FNetIfaceSend      fSend;      // Send message function
-  FNetIfaceRecv      fRecv;      // Recv message function
-  FNetLinkDestroy    fDestroy;
+  void             * user_data;
+  net_iface_ops_t    ops;
 
 #ifdef OSPF_SUPPORT
   ospf_area_t      tArea;  
 #endif
-} SNetIface;
-typedef SNetIface SNetLink;
+} net_iface_t;
 
 
 //////////////////////////////////////////////////////////////////////
@@ -307,8 +331,21 @@ typedef SNetIface SNetLink;
  * recorded node as context.
  */
 typedef struct {
-  SNetIface * pIface;
-  SNetMessage * pMessage;
-} SNetSendContext;
+  net_iface_t * dst_iface;
+  net_msg_t   * msg;
+} net_send_ctx_t;
+
+typedef enum {
+  NODE,
+  SUBNET
+} net_elem_type_t;
+
+typedef struct {
+  net_elem_type_t   type;      /* item type: node / subnet */
+  union {
+    net_node_t      * node;
+    net_subnet_t    * subnet;
+  };
+} net_elem_t;
 
 #endif /* __NET_TYPES_H__ */
