56fefe545e06d5f46fb4c413be8fd40ba3b76b05
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2008-04-07 09:06:37 +0000

    Changes: - cosmetic changes (naming guidelines) - SBGPPeer -> bgp_peer_t - session states defined as en enumeration (enum) -> bgp_peer_state_t

--- src/bgp/peer.h
+++ src/bgp/peer.h
@@ -29,12 +29,11 @@ struct TBGPPeer {
 			      // IP address when the session is
 			      // opened.
   uint16_t uRemoteAS;         // AS-number of the neighbor.
-  SBGPRouter * pLocalRouter;  // Reference to the local router.
-  SFilter * pInFilter;        // Input and output filters
-  SFilter * pOutFilter;
-  SRIB * pAdjRIBIn;           // Input and output Adj-RIB
-  SRIB * pAdjRIBOut;
-  uint8_t uSessionState;      // Session state (handled by the FSM)
+  bgp_router_t * pLocalRouter;  // Reference to the local router.
+  bgp_filter_t * pFilter[FILTER_MAX]; // Input and output filters
+  bgp_rib_t    * pAdjRIB[RIB_MAX];    // Input and output Adj-RIBs
+  bgp_peer_state_t tSessionState; // Session state (handled by the FSM)
+  
   uint8_t uFlags;             // Configuration flags
   net_addr_t tNextHop;        // BGP next-hop to advertise to this
 			      // peer.
@@ -44,6 +43,8 @@ struct TBGPPeer {
 #endif
   unsigned int uSendSeqNum;
   unsigned int uRecvSeqNum;
+
+  int last_error;
 };
 
 #ifdef __cplusplus
@@ -51,66 +52,64 @@ extern "C" {
 #endif
 
   // ----- bgp_peer_create ------------------------------------------
-  SBGPPeer * bgp_peer_create(uint16_t uRemoteAS,
+  bgp_peer_t * bgp_peer_create(uint16_t uRemoteAS,
 			     net_addr_t tAddr,
-			     SBGPRouter * pLocalRouter);
+			     bgp_router_t * pLocalRouter);
   // ----- bgp_peer_destroy -----------------------------------------
-  void bgp_peer_destroy(SBGPPeer ** ppPeer);
+  void bgp_peer_destroy(bgp_peer_t ** ppPeer);
 
   // ----- bgp_peer_flag_set ----------------------------------------
-  void bgp_peer_flag_set(SBGPPeer * pPeer, uint8_t uFlag, int iState);
+  void bgp_peer_flag_set(bgp_peer_t * pPeer, uint8_t uFlag, int iState);
   // ----- bgp_peer_flag_get ----------------------------------------
-  int bgp_peer_flag_get(SBGPPeer * pPeer, uint8_t uFlag);
+  int bgp_peer_flag_get(bgp_peer_t * pPeer, uint8_t uFlag);
   // ----- bgp_peer_set_nexthop -------------------------------------
-  int bgp_peer_set_nexthop(SBGPPeer * pPeer, net_addr_t tNextHop);
+  int bgp_peer_set_nexthop(bgp_peer_t * pPeer, net_addr_t tNextHop);
   // -----[ bgp_peer_set_source ]------------------------------------
-  int bgp_peer_set_source(SBGPPeer * pPeer, net_addr_t tSrcAddr);
+  int bgp_peer_set_source(bgp_peer_t * pPeer, net_addr_t tSrcAddr);
 
   ///////////////////////////////////////////////////////////////////
   // BGP FILTERS
   ///////////////////////////////////////////////////////////////////
   
-  // ----- bgp_peer_set_in_filter -----------------------------------
-  void bgp_peer_set_in_filter(SBGPPeer * pPeer, SFilter * pFilter);
-  // ----- bgp_peer_in_filter_get -----------------------------------
-  SFilter * bgp_peer_in_filter_get(SBGPPeer * pPeer);
-  // ----- bgp_peer_set_out_filter ----------------------------------
-  void bgp_peer_set_out_filter(SBGPPeer * pPeer, SFilter * pFilter);
-  // ----- bgp_peer_out_filter_get ----------------------------------
-  SFilter * bgp_peer_out_filter_get(SBGPPeer * pPeer);
+  // -----[ bgp_peer_set_filter ]------------------------------------
+  void bgp_peer_set_filter(bgp_peer_t * pPeer, bgp_filter_dir_t dir,
+			   bgp_filter_t * pFilter);
+  // -----[ bgp_peer_filter_get ]------------------------------------
+  bgp_filter_t * bgp_peer_filter_get(bgp_peer_t * pPeer,
+				     bgp_filter_dir_t dir);
 
   ///////////////////////////////////////////////////////////////////
   // BGP SESSION MANAGEMENT FUNCTIONS
   ///////////////////////////////////////////////////////////////////
 
   // ----- bgp_peer_session_ok --------------------------------------
-  int bgp_peer_session_ok(SBGPPeer * pPeer);
+  int bgp_peer_session_ok(bgp_peer_t * pPeer);
   // ----- bgp_peer_session_refresh ---------------------------------
-  void bgp_peer_session_refresh(SBGPPeer * pPeer);
+  void bgp_peer_session_refresh(bgp_peer_t * pPeer);
   // ----- peer_open_session ----------------------------------------
-  int bgp_peer_open_session(SBGPPeer * pPeer);
+  int bgp_peer_open_session(bgp_peer_t * pPeer);
   // ----- peer_close_session ---------------------------------------
-  int bgp_peer_close_session(SBGPPeer * pPeer);
+  int bgp_peer_close_session(bgp_peer_t * pPeer);
   // ----- peer_rescan_adjribin -------------------------------------
-  void peer_rescan_adjribin(SBGPPeer * pPeer, int iClear);
+  void peer_rescan_adjribin(bgp_peer_t * pPeer, int iClear);
 
   ///////////////////////////////////////////////////////////////////
   // BGP MESSAGE HANDLING
   ///////////////////////////////////////////////////////////////////
 
   // ----- bgp_peer_announce_route ----------------------------------
-  void bgp_peer_announce_route(SBGPPeer * pPeer, SRoute * pRoute);
+  void bgp_peer_announce_route(bgp_peer_t * pPeer, SRoute * pRoute);
   // ----- bgp_peer_withdraw_prefix ---------------------------------
-  void bgp_peer_withdraw_prefix(SBGPPeer * pPeer, SPrefix sPrefix);
+  void bgp_peer_withdraw_prefix(bgp_peer_t * pPeer, SPrefix sPrefix);
   // ----- bgp_peer_handle_message ----------------------------------
-  int bgp_peer_handle_message(SBGPPeer * pPeer, SBGPMsg * pMsg);
+  int bgp_peer_handle_message(bgp_peer_t * pPeer, SBGPMsg * pMsg);
   // -----[ bgp_peer_send ]------------------------------------------
-  int bgp_peer_send(SBGPPeer * pPeer, SBGPMsg * pMsg);
+  int bgp_peer_send(bgp_peer_t * pPeer, SBGPMsg * pMsg);
   
   // ----- bgp_peer_route_eligible ----------------------------------
-  int bgp_peer_route_eligible(SBGPPeer * pPeer, SRoute * pRoute);
+  int bgp_peer_route_eligible(bgp_peer_t * pPeer, SRoute * pRoute);
   // ----- bgp_peer_route_feasible ----------------------------------
-  int bgp_peer_route_feasible(SBGPPeer * pPeer, SRoute * pRoute);
+  int bgp_peer_route_feasible(bgp_peer_t * pPeer, SRoute * pRoute);
 
 
   ///////////////////////////////////////////////////////////////////
@@ -118,34 +117,32 @@ extern "C" {
   ///////////////////////////////////////////////////////////////////
   
   // ----- bgp_peer_dump_id -----------------------------------------
-  void bgp_peer_dump_id(SLogStream * pStream, SBGPPeer * pPeer);
+  void bgp_peer_dump_id(SLogStream * pStream, bgp_peer_t * pPeer);
   // ----- bgp_peer_dump --------------------------------------------
-  void bgp_peer_dump(SLogStream * pStream, SBGPPeer * pPeer);
+  void bgp_peer_dump(SLogStream * pStream, bgp_peer_t * pPeer);
   // ----- bgp_peer_dump_adjrib -------------------------------------
-  void bgp_peer_dump_adjrib(SLogStream * pStream, SBGPPeer * pPeer,
-				   SPrefix sPrefix, int iInOut);
-  // ----- bgp_peer_dump_in_filters ---------------------------------
-  void bgp_peer_dump_in_filters(SLogStream * pStream, SBGPPeer * pPeer);
-  // ----- bgp_peer_dump_out_filters --------------------------------
-  void bgp_peer_dump_out_filters(SLogStream * pStream, SBGPPeer * pPeer);
-  
+  void bgp_peer_dump_adjrib(SLogStream * pStream, bgp_peer_t * pPeer,
+				   SPrefix sPrefix, bgp_rib_dir_t dir);
+  // ----- bgp_peer_dump_filters ---------------------------------
+  void bgp_peer_dump_filters(SLogStream * pStream, bgp_peer_t * pPeer,
+			     bgp_filter_dir_t dir);
   // -----[ bgp_peer_set_record_stream ]-----------------------------
-  int bgp_peer_set_record_stream(SBGPPeer * pPeer,
+  int bgp_peer_set_record_stream(bgp_peer_t * pPeer,
 				 SLogStream * pStream);
   // -----[ bgp_peer_send_enabled ]----------------------------------
-  int bgp_peer_send_enabled(SBGPPeer * pPeer);
+  int bgp_peer_send_enabled(bgp_peer_t * pPeer);
 
 
   ///////////////////////////////////////////////////////////////////
   // WALTON DRAFT (EXPERIMENTAL)
   ///////////////////////////////////////////////////////////////////
 #if defined __EXPERIMENTAL__ && defined __EXPERIMENTAL_WALTON__
-  void bgp_peer_withdraw_prefix_walton(SBGPPeer * pPeer, SPrefix sPrefix,
+  void bgp_peer_withdraw_prefix_walton(bgp_peer_t * pPeer, SPrefix sPrefix,
 				       net_addr_t * tNextHop);
   // -----[ bgp_peer_set_walton_limit ]------------------------------
-  void bgp_peer_set_walton_limit(SBGPPeer * pPeer, unsigned int uWaltonLimit);
+  void bgp_peer_set_walton_limit(bgp_peer_t * pPeer, unsigned int uWaltonLimit);
   // -----[ bgp_peer_get_walton_limit ]------------------------------
-  uint16_t bgp_peer_get_walton_limit(SBGPPeer * pPeer);
+  uint16_t bgp_peer_get_walton_limit(bgp_peer_t * pPeer);
 #endif
 
 
