e8599b7a93d996858ca3784180b51803d8fce26f
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2008-04-07 09:34:33 +0000

    Changes: - record-route defined here instead of in record-route.c (which will be deprecated in the future) - ICMP options moved to icmp_options.c/.h - cosmetic changes (naming guidelines)

--- src/net/icmp.h
+++ src/net/icmp.h
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 25/02/2004
-// @lastdate 02/03/2008
+// @lastdate 10/03/2008
 // ==================================================================
 
 #ifndef __NET_ICMP_H__
@@ -14,46 +14,73 @@
 #include <net/message.h>
 #include <net/network.h>
 #include <net/net_path.h>
+#include <net/icmp_options.h>
 
-#define ICMP_ERROR_NET_UNREACH    1
-#define ICMP_ERROR_HOST_UNREACH   2
-#define ICMP_ERROR_PROTO_UNREACH  3
-#define ICMP_ERROR_TIME_EXCEEDED  4
+typedef enum {
+  ICMP_ERROR_NET_UNREACH  = 1,
+  ICMP_ERROR_HOST_UNREACH = 2,
+  ICMP_ERROR_PROTO_UNREACH= 3,
+  ICMP_ERROR_TIME_EXCEEDED= 4,
+} icmp_error_type_t;
+
+// ----- ICMP message types -----
+typedef enum {
+  ICMP_ECHO_REQUEST= 0,
+  ICMP_ECHO_REPLY  = 1,
+  ICMP_ERROR       = 2,
+  ICMP_TRACE       = 3, /* This is a non-standard type used to perform
+			 * "record-route" actions */
+} icmp_msg_type_t;
+
+typedef struct {
+  icmp_msg_type_t         type;     /* ICMP type */
+  icmp_error_type_t       sub_type; /* ICMP subtype (error) */
+  net_node_t            * node;     /* Origin node */
+  struct icmp_msg_opt_t   opts;     /* Options */
+} icmp_msg_t;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
   // -----[ icmp_send_error ]----------------------------------------
-  int icmp_send_error(SNetNode * pNode, net_addr_t tSrcAddr,
-		      net_addr_t tDstAddr, uint8_t uErrorCode,
-		      SSimulator * pSimulator);
+  int icmp_send_error(net_node_t * node, net_addr_t src_addr,
+		      net_addr_t dst_addr, icmp_error_type_t tErrorCode,
+		      simulator_t * sim);
   // -----[ icmp_send_echo_request ]---------------------------------
-  int icmp_send_echo_request(SNetNode * pNode, net_addr_t tSrcAddr,
-			net_addr_t tDstAddr, uint8_t uTTL,
-			SSimulator * pSimulator);
+  int icmp_send_echo_request(net_node_t * node, net_addr_t src_addr,
+			     net_addr_t dst_addr, uint8_t ttl,
+			     simulator_t * sim);
   // -----[ icmp_event_handler ]-------------------------------------
-  int icmp_event_handler(SSimulator * pSimulator,
-			 void * pHandler, SNetMessage * pMessage);
+  int icmp_event_handler(simulator_t * sim,
+			 void * handler, net_msg_t * msg);
   // -----[ is_icmp_error ]------------------------------------------
-  int is_icmp_error(SNetMessage * pMessage);
+  int is_icmp_error(net_msg_t * msg);
   // -----[ icmp_ping_send_recv ]------------------------------------
-  int icmp_ping_send_recv(SNetNode * pSrcNode, net_addr_t tSrcAddr,
-			  net_addr_t tDstAddr, uint8_t uTTL);
+  int icmp_ping_send_recv(net_node_t * node, net_addr_t src_addr,
+			  net_addr_t dst_addr, uint8_t ttl);
   
   ///////////////////////////////////////////////////////////////////
   // PROBES
   ///////////////////////////////////////////////////////////////////
 
   // -----[ icmp_ping ]----------------------------------------------
-  int icmp_ping(SLogStream * pStream,
-		SNetNode * pSrcNode, net_addr_t tSrcAddr,
-		net_addr_t tDstAddr, uint8_t uTTL);
+  int icmp_ping(SLogStream * stream,
+		net_node_t * node, net_addr_t src_addr,
+		net_addr_t dst_addr, uint8_t ttl);
   // -----[ icmp_trace_route ]---------------------------------------
-  int icmp_trace_route(SLogStream * pStream,
-		       SNetNode * pSrcNode, net_addr_t tSrcAddr,
-		       net_addr_t tDstAddr, uint8_t uMaxTTL,
-		       SIPTrace * pTrace);
+  int icmp_trace_route(SLogStream * stream,
+		       net_node_t * node, net_addr_t src_addr,
+		       net_addr_t dst_addr, uint8_t max_ttl,
+		       ip_trace_t ** trace_ref);
+  // -----[ icmp_record_route ]--------------------------------------
+  int icmp_record_route(net_node_t * node,
+			net_addr_t dst_addr,
+			SPrefix * alt_dest,
+			uint8_t max_ttl,
+			uint8_t options,
+			ip_trace_t ** trace_ref,
+			net_link_load_t load);
   
 #ifdef __cplusplus
 }
