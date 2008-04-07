763f67eafb243b022efb6f937b4efb467fe7c6dd
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2008-04-07 09:08:13 +0000

    Changes: - cosmetic changes (naming guidelines) - RIB direction (input/output) defined as an enumeration (enum) -> bgp_rib_dir_t - SRIB -> bgp_rib_t

--- trunk/src/bgp/rib_t.h
+++ trunk/src/bgp/rib_t.h
@@ -1,9 +1,9 @@
 // ==================================================================
 // @(#)rib_t.h
 //
-// @author Bruno Quoitin (bqu@info.ucl.ac.be)
+// @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 04/12/2002
-// @lastdate 21/05/2007
+// @lastdate 10/03/2008
 // ==================================================================
 
 #ifndef __RIB_T_H__
@@ -11,6 +11,13 @@
 
 #include <libgds/patricia-tree.h>
 
+typedef enum {
+  RIB_IN,
+  RIB_OUT,
+  RIB_MAX
+} bgp_rib_dir_t;
+
 typedef STrie SRIB;
+typedef STrie bgp_rib_t;
 
 #endif
