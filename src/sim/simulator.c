918442533e331db360151c7fa911159394e1a464
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2009-03-09 16:44:52 +0000

    Added: - function sim_is_running() to test if simulator is currently running.

--- src/sim/simulator.c
+++ src/sim/simulator.c
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 13/06/2003
-// $Id: simulator.c,v 1.13 2008-04-14 09:12:14 bqu Exp $
+// $Id: simulator.c,v 1.14 2009-03-09 16:44:52 bqu Exp $
 // ==================================================================
 
 #ifdef HAVE_CONFIG_H
@@ -13,28 +13,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <libgds/memory.h>
+
 #include <net/error.h>
 #include <sim/simulator.h>
 #include <sim/scheduler.h>
 #include <sim/static_scheduler.h>
 
-#define SIMULATOR_TIME_STEP 0.01
+//#define DEBUG
+#include <libgds/debug.h>
 
 sched_type_t SIM_OPTIONS_SCHEDULER= SCHEDULER_STATIC;
+
+// -----[ sim_sched_factory_f ]--------------------------------------
+typedef sched_t * (*sim_sched_factory_f)(simulator_t * sim);
+
+static struct {
+  const char          * name;
+  sim_sched_factory_f   factory;
+} SCHEDULERS[SCHEDULER_MAX]= {
+  { "static",  sched_static_create },
+  { "dynamic", sched_dynamic_create },
+};
 
 // -----[ sim_create ]-----------------------------------------------
 simulator_t * sim_create(sched_type_t type)
 {
-  simulator_t * sim= (simulator_t *) MALLOC(sizeof(simulator_t));
-
-  switch (type) {
-  case SCHEDULER_STATIC:
-    sim->sched= sched_static_create(sim);
-    break;
-  default:
-    fatal("The requested scheduler type (%d) is not supported", type);
-  }
+  simulator_t * sim;
+  
+  if (type >= SCHEDULER_MAX)
+    cbgp_fatal("The requested scheduler type (%d) is not supported", type);
+  
+  sim= (simulator_t *) MALLOC(sizeof(simulator_t));
   sim->max_time= 0;
+  sim->sched= SCHEDULERS[type].factory(sim);
+  sim->running= 0;
   return sim;
 }
 
@@ -48,6 +61,12 @@ void sim_destroy(simulator_t ** sim_ref)
   }
 }
 
+// -----[ sim_cancel ]-----------------------------------------------
+void sim_cancel(simulator_t * sim)
+{
+  sim->sched->ops.cancel(sim->sched);
+}
+
 // -----[ sim_clear ]------------------------------------------------
 void sim_clear(simulator_t * sim)
 {
@@ -55,27 +74,28 @@ void sim_clear(simulator_t * sim)
 }
 
 // -----[ sim_run ]--------------------------------------------------
-/**
- *
- */
 int sim_run(simulator_t * sim)
 {
-  return sim->sched->ops.run(sim->sched, -1);
+  int result;
+  sim->running= 1;
+  result= sim->sched->ops.run(sim->sched, -1);
+  sim->running= 0;
+  return result;
+}
+
+// -----[ sim_is_running ]-----------------------------------------
+int sim_is_running(simulator_t * sim)
+{
+  return sim->running;
 }
 
 // -----[ sim_step ]-------------------------------------------------
-/**
- *
- */
 int sim_step(simulator_t * sim, unsigned int num_steps)
 {
   return sim->sched->ops.run(sim->sched, num_steps);
 }
 
 // -----[ sim_get_num_events ]---------------------------------------
-/**
- *
- */
 uint32_t sim_get_num_events(simulator_t * sim)
 {
   return sim->sched->ops.num_events(sim->sched);
@@ -88,9 +108,6 @@ void * sim_get_event(simulator_t * sim, unsigned int index)
 }
 
 // -----[ sim_post_event ]-------------------------------------------
-/**
- *
- */
 int sim_post_event(simulator_t * sim, sim_event_ops_t * ops,
 		   void * ctx, double time, sim_time_t time_type)
 {
@@ -98,52 +115,69 @@ int sim_post_event(simulator_t * sim, sim_event_ops_t * ops,
 }
 
 // -----[ sim_get_time ]---------------------------------------------
-/**
- *
- */
 double sim_get_time(simulator_t * sim)
 {
   return sim->sched->ops.cur_time(sim->sched);
 }
 
 // -----[ sim_set_max_time ]-----------------------------------------
-/**
- *
- */
 void sim_set_max_time(simulator_t * sim, double max_time)
 {
   sim->max_time= max_time;
 }
 
 // -----[ sim_dump_events ]------------------------------------------
-/**
- *
- */
-void sim_dump_events(SLogStream * stream, simulator_t * sim)
+void sim_dump_events(gds_stream_t * stream, simulator_t * sim)
 {
   sim->sched->ops.dump_events(stream, sim->sched);
 }
 
 // -----[ sim_show_infos ]-------------------------------------------
-/**
- *
- */
-void sim_show_infos(SLogStream * stream, simulator_t * sim)
+void sim_show_infos(gds_stream_t * stream, simulator_t * sim)
 {
-  log_printf(stream, "scheduler   : ");
-  switch (sim->sched->type) {
-  case SCHEDULER_STATIC:
-    log_printf(stream, "static"); break;
-  default:
-    log_printf(stream, "unknown");
-  }
-  log_printf(stream, "\n");
-  log_printf(stream, "current time: %f\n", sim_get_time(sim));
-  log_printf(stream, "maximum time: %f\n", sim->max_time);
+  stream_printf(stream, "scheduler   : %s\n",
+		sim_sched2str(sim->sched->type));
+  stream_printf(stream, "current time: %f\n", sim_get_time(sim));
+  stream_printf(stream, "maximum time: %f\n", sim->max_time);
 }
 
 // -----[ sim_set_log_progress ]-------------------------------------
-void sim_set_log_progress(simulator_t * sim, const char * file_name)
+void sim_set_log_progress(simulator_t * sim, const char * filename)
 {
-  sim->sched->ops.set_log_process(sim->sched, file_name);
+  sim->sched->ops.set_log_process(sim->sched, filename);
 }
+
+// -----[ sim_sched2str ]--------------------------------------------
+const char * sim_sched2str(sched_type_t type)
+{
+  if (type >= SCHEDULER_MAX)
+    return NULL;
+  return SCHEDULERS[type].name;
+}
+
+// -----[ sim_str2sched ]--------------------------------------------
+int sim_str2sched(const char * str, sched_type_t * type_ref)
+{
+  sched_type_t type;
+  for (type= 0; type < SCHEDULER_MAX; type++)
+    if (!strcmp(str, SCHEDULERS[type].name)) {
+      *type_ref= type;
+      return 0;
+    }
+  return -1;
+}
+
+static sched_type_t _default_sched= SCHEDULER_STATIC;
+
+// -----[ sim_set_default_scheduler ]------------------------------
+void sim_set_default_scheduler(sched_type_t type)
+{
+  _default_sched= type;
+}
+
+// -----[ sim_get_default_scheduler ]------------------------------
+sched_type_t sim_get_default_scheduler()
+{
+  return _default_sched;
+}
+
