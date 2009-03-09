226071021a4508094d68a5400afc77193a48fcd8
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2009-03-09 16:44:19 +0000

    Added: - signal handler to allow stopping running simulation without exiting c-bgp.

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -4,7 +4,7 @@
 // @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @author Sebastien Tandel (standel@info.ucl.ac.be)
 // @date 22/11/2002
-// $Id: main.c,v 1.37 2008-06-16 09:58:24 bqu Exp $
+// $Id: main.c,v 1.38 2009-03-09 16:44:19 bqu Exp $
 // ==================================================================
 
 #ifdef HAVE_CONFIG_H
@@ -24,6 +24,8 @@
 #include <libgds/str_util.h>
 
 #include <cli/common.h>
+#include <net/network.h>
+#include <sim/simulator.h>
 
 //#include <net/ospf.h>
 
@@ -35,8 +37,32 @@
 #define CBGP_MODE_OSPF        4
 
 // -----[ global options ]-----
-uint8_t uMode   = CBGP_MODE_DEFAULT;
-char * pcArgMode= NULL;
+uint8_t mode   = CBGP_MODE_DEFAULT;
+char * arg_mode= NULL;
+
+// -----[ signal_handler ]-------------------------------------------
+/**
+ * This is an ANSI C signal handler for SIGINT. The signal handler is
+ * used to interrupt running simulations without quitting C-BGP.
+ */
+void signal_handler(int signum)
+{
+  network_t * network;
+  simulator_t * sim;
+
+  if (signum != SIGINT)
+    return;
+
+  network= network_get_default();
+  if (network == NULL)
+    return;
+
+  sim= network_get_simulator(network);
+  if ((sim != NULL) && sim_is_running(sim)) {
+    cbgp_warn("Simulation interrupted by user.\n");
+    sim_cancel(sim);
+  }
+}
 
 // -----[ simulation_cli_help ]--------------------------------------
 /**
@@ -48,18 +74,16 @@ void simulation_cli_help()
   printf("\nUsage: cbgp [OPTIONS]\n");
   printf("\n");
   printf("  -h             display this message.\n");
-  printf("  -i             interactive mode.\n");
   printf("  -l LOGFILE     output log to LOGFILE instead of stderr.\n");
   printf("  -c SCRIPT      load and execute SCRIPT file.\n");
   printf("                 (without this option, commands are taken from stdin)\n");
   printf("  -e COMMAND     execute the given command\n");
-#if defined(HAVE_MEM_FLAG_SET) && (HAVE_MEM_FLAG_SET == 1)
-  printf("  -g             track memory leaks.\n");
-#endif
+  printf("  -D param=value defines a parameter\n");
 
 #ifdef OSPF_SUPPORT
   printf("  -o             test OSPF model (cbgp must be compiled with --enable-ospf option).\n");
 #endif
+
   printf("\n");
 }
 
@@ -67,53 +91,28 @@ void simulation_cli_help()
 /**
  * Change the simulation mode.
  */
-void simulation_set_mode(uint8_t uNewMode, char * pcNewArg)
+void simulation_set_mode(uint8_t new_mode, const char * new_arg)
 {
-  if (uMode == CBGP_MODE_DEFAULT) {
-    uMode= uNewMode;
-    if (pcNewArg != NULL)
-      pcArgMode= pcNewArg;
-  } else {
+  if (mode != CBGP_MODE_DEFAULT) {
     fprintf(stderr, "Error: multiple modes specified on command-line.\n");
     simulation_cli_help();
     exit(EXIT_FAILURE);
   }
-}
-
-// ----- option_string ----------------------------------------------
-/**
- *
- */
-char * option_string(char * pcArgument)
-{
-  return str_create(pcArgument);
-}
-
-// ----- option_free ------------------------------------------------
-/**
- *
- */
-void option_free(char * pcArgument)
-{
-  if (pcArgument != NULL)
-    str_destroy(&pcArgument);
+   
+  mode= new_mode;
+  if (new_arg != NULL)
+    arg_mode= str_create(new_arg);
 }
 
 /////////////////////////////////////////////////////////////////////
 // INITIALIZATION AND FINALIZATION SECTION
 /////////////////////////////////////////////////////////////////////
 
-void _main_init() __attribute((constructor));
 void _main_done() __attribute((destructor));
-
-void _main_init()
-{
-  libcbgp_init();
-}
 
 void _main_done()
 {
-  option_free(pcArgMode);
+  str_destroy(&arg_mode);
   libcbgp_done();
 }
 
@@ -128,32 +127,35 @@ void _main_done()
  * This is C-BGP's main entry point.
  */
 int main(int argc, char ** argv) {
-  int iResult;
-  int iExitCode= EXIT_SUCCESS;
+  int result;
+  int exit_code= EXIT_SUCCESS;
+  gds_tokenizer_t * tokenizer;
+  const gds_tokens_t * tokens;
+  struct sigaction sa;
 
-  /* Process command-line options */
-  while ((iResult= getopt(argc, argv, "mc:e:hil:got:")) != -1) {
-    switch (iResult) {
+  libcbgp_init(argc, argv);
+
+  // Process command-line options
+  while ((result= getopt(argc, argv, "mc:D:e:hil:ot:")) != -1) {
+    switch (result) {
     case 'c':
-      simulation_set_mode(CBGP_MODE_SCRIPT, option_string(optarg));
+      simulation_set_mode(CBGP_MODE_SCRIPT, optarg);
+      break;
+    case 'D':
+      tokenizer= tokenizer_create("=", NULL, NULL);
+      assert(tokenizer_run(tokenizer, optarg) == TOKENIZER_SUCCESS);
+      tokens= tokenizer_get_tokens(tokenizer);
+      assert(tokens_get_num(tokens) == 2);
+      libcbgp_set_param(tokens_get_string_at(tokens, 0),
+			tokens_get_string_at(tokens, 1));
+      tokenizer_destroy(&tokenizer);
       break;
     case 'e':
-      simulation_set_mode(CBGP_MODE_EXECUTE, option_string(optarg));
-      break;
-    case 'g':
-#if defined(HAVE_MEM_FLAG_SET) && (HAVE_MEM_FLAG_SET == 1)
-      //mem_flag_set(MEM_FLAG_WARN_LEAK, 1);
-#else
-      fprintf(stderr, "Error: option -g not supported (check your libgds version).\n");
-      exit(EXIT_FAILURE);
-#endif
+      simulation_set_mode(CBGP_MODE_EXECUTE, optarg);
       break;
     case 'h':
       simulation_cli_help();
       exit(EXIT_SUCCESS);
-      break;
-    case 'i':
-      simulation_set_mode(CBGP_MODE_INTERACTIVE, NULL);
       break;
     case 'l':
       libcbgp_set_debug_file(optarg);
@@ -169,29 +171,37 @@ int main(int argc, char ** argv) {
     }
   }
 
+  if ((mode == CBGP_MODE_DEFAULT) && isatty(0))
+    simulation_set_mode(CBGP_MODE_INTERACTIVE, NULL);
+
+  sa.sa_handler= signal_handler;
+  sa.sa_flags= 0;
+  sa.sa_mask= 0;
+  assert(sigaction(SIGINT, &sa, NULL) >= 0);
+
   /* Run simulation in selected mode... */
-  switch (uMode) {
+  switch (mode) {
   case CBGP_MODE_DEFAULT:
     if (libcbgp_exec_stream(stdin) != CLI_SUCCESS)
-      iExitCode= EXIT_FAILURE;
+      exit_code= EXIT_FAILURE;
     break;
   case CBGP_MODE_INTERACTIVE:
     if (libcbgp_interactive() != CLI_SUCCESS)
-      iExitCode= EXIT_FAILURE;
+      exit_code= EXIT_FAILURE;
     break;
   case CBGP_MODE_SCRIPT:
-    iExitCode= (libcbgp_exec_file(pcArgMode) == 0)?
+    exit_code= (libcbgp_exec_file(arg_mode) == 0)?
       EXIT_SUCCESS:EXIT_FAILURE;
     break;
   case CBGP_MODE_EXECUTE:
-    iExitCode= (libcbgp_exec_cmd(pcArgMode) == 0)?
+    exit_code= (libcbgp_exec_cmd(arg_mode) == 0)?
       EXIT_SUCCESS:EXIT_FAILURE;
     break;
 
   case CBGP_MODE_OSPF:
 #ifdef OSPF_SUPPORT
-    iExitCode= ospf_test();
-    if (iExitCode != 0)
+    exit_code= ospf_test();
+    if (exit_code != 0)
       printf("ospf_test ok!\n");
     else 
       printf("ospf_test has failed :-(\n");
@@ -201,5 +211,5 @@ int main(int argc, char ** argv) {
     break;
   }
 
-  return iExitCode;
+  return exit_code;
 }
