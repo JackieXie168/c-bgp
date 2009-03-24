113a4524d4a3fbbd6f02b9c4c161751024b7a6c9
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2009-03-24 16:23:40 +0000

    Changes: - cosmetic changes - static definition of protocols imported from ipip.c, bgp.c, icmp.c

--- trunk/src/net/protocol.c
+++ trunk/src/net/protocol.c
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 25/02/2004
-// $Id: protocol.c,v 1.7 2008-06-13 14:26:23 bqu Exp $
+// $Id: protocol.c,v 1.8 2009-03-24 16:23:40 bqu Exp $
 // ==================================================================
 
 #ifdef HAVE_CONFIG_H
@@ -20,12 +20,26 @@
 #include <net/net_types.h>
 #include <net/protocol.h>
 
-// -----[ Protocol names ]-----
-const char * PROTOCOL_NAMES[NET_PROTOCOL_MAX]= {
-  "raw",
-  "icmp",
-  "bgp",
-  "ipip"
+#include <net/icmp.h>
+#include <net/ipip.h>
+#include <bgp/as.h>
+
+const net_protocol_def_t PROTOCOL_RAW= {
+  .name= "raw",
+  .ops= {
+    .handle      = NULL,
+    .destroy     = NULL,
+    .dump_msg    = NULL,
+    .destroy_msg = NULL,
+    .copy_payload= NULL,
+  }
+};
+
+const net_protocol_def_t * PROTOCOL_DEFS[NET_PROTOCOL_MAX]= {
+  &PROTOCOL_RAW,
+  &PROTOCOL_ICMP,
+  &PROTOCOL_BGP,
+  &PROTOCOL_IPIP,
 };
 
 // -----[ _protocol_create ]-----------------------------------------
@@ -34,19 +48,14 @@ const char * PROTOCOL_NAMES[NET_PROTOCOL_MAX]= {
  */
 static inline
 net_protocol_t * _protocol_create(net_protocol_id_t id,
-				  const char * name,
 				  void * handler,
-				  FNetProtoHandlerDestroy destroy,
-				  FNetProtoHandleEvent handle_event)
+				  const net_protocol_def_t * def)
 {
   net_protocol_t * proto=
     (net_protocol_t *) MALLOC(sizeof(net_protocol_t));
   proto->id= id;
-  proto->name= name;
   proto->handler= handler;
-  proto->ops.handle= handle_event;
-  proto->ops.destroy= destroy;
-  proto->ops.dump= NULL;
+  proto->def= def;
   return proto;
 }
 
@@ -58,8 +67,8 @@ static inline
 void _protocol_destroy(net_protocol_t ** proto_ref)
 {
   if (*proto_ref != NULL) {
-    if ((*proto_ref)->ops.destroy != NULL)
-      (*proto_ref)->ops.destroy(&(*proto_ref)->handler);
+    if ((*proto_ref)->def->ops.destroy != NULL)
+      (*proto_ref)->def->ops.destroy(&(*proto_ref)->handler);
     FREE(*proto_ref);
     *proto_ref= NULL;
   }
@@ -69,12 +78,15 @@ void _protocol_destroy(net_protocol_t ** proto_ref)
 net_error_t protocol_recv(net_protocol_t * proto, net_msg_t * msg,
 			  simulator_t * sim)
 {
-  assert(proto->ops.handle != NULL);
-  return proto->ops.handle(sim, proto->handler, msg);
+  //assert(proto->def->ops.handle != NULL);
+  if (proto->def->ops.handle != NULL)
+    return proto->def->ops.handle(sim, proto->handler, msg);
+  return ESUCCESS;
 }
 
 // -----[ _protocols_cmp ]-------------------------------------------
-static int _protocols_cmp(void * item1, void * item2,
+static int _protocols_cmp(const void * item1,
+			  const void * item2,
 			  unsigned int item_size)
 {
   net_protocol_t * proto1= *((net_protocol_t **) item1);
@@ -88,26 +100,24 @@ static int _protocols_cmp(void * item1, void * item2,
 }
 
 // -----[ _protocols_destroy ]---------------------------------------
-static void _protocols_destroy(void * item)
+static void _protocols_destroy(void * item, const void * ctx)
 {
   _protocol_destroy((net_protocol_t **) item);
 }
 
-// ----- protocols_create -------------------------------------------
-/**
- *
- */
+// -----[ protocols_create ]-----------------------------------------
 net_protocols_t * protocols_create()
 {
-  net_protocols_t * pProtocols=
+  net_protocols_t * protocols=
     (net_protocols_t *) ptr_array_create(ARRAY_OPTION_SORTED|
 					 ARRAY_OPTION_UNIQUE,
 					 _protocols_cmp,
-					 _protocols_destroy);
-  return pProtocols;
+					 _protocols_destroy,
+					 NULL);
+  return protocols;
 }
 
-// ----- protocols_destroy ------------------------------------------
+// -----[ protocols_destroy ]----------------------------------------
 /**
  * This function destroys the list of protocols and destroys all the
  * related protocol instances if required.
@@ -117,18 +127,20 @@ void protocols_destroy(net_protocols_t ** protocols_ref)
   ptr_array_destroy(protocols_ref);
 }
 
-// ----- protocols_register -----------------------------------------
+// -----[ protocols_add ]--------------------------------------------
 /**
  * Add a protocol handler to the protocol list.
  */
-net_error_t protocols_register(net_protocols_t * protocols,
-			       net_protocol_id_t id,
-			       void * handler,
-			       FNetProtoHandlerDestroy destroy,
-			       FNetProtoHandleEvent handle_event)
+net_error_t protocols_add(net_protocols_t * protocols,
+			  net_protocol_id_t id,
+			  void * handler)
 {
-  net_protocol_t * protocol=
-    _protocol_create(id, NULL, handler, destroy, handle_event);
+  net_protocol_t * protocol;
+
+  if (id >= NET_PROTOCOL_MAX)
+    return ENET_PROTO_UNKNOWN;
+
+  protocol= _protocol_create(id, handler, PROTOCOL_DEFS[id]);
 
   if (ptr_array_add(protocols, &protocol) < 0) {
     _protocol_destroy(&protocol);
@@ -138,7 +150,7 @@ net_error_t protocols_register(net_protocols_t * protocols,
   return ESUCCESS;
 }
 
-// ----- protocols_get ----------------------------------------------
+// -----[ protocols_get ]--------------------------------------------
 /**
  *
  */
@@ -159,7 +171,7 @@ const char * net_protocol2str(net_protocol_id_t id)
 {
   if (id >= NET_PROTOCOL_MAX)
     return "unknown";
-  return PROTOCOL_NAMES[id];
+  return PROTOCOL_DEFS[id]->name;
 }
 
 // -----[ net_str2protocol ]-----------------------------------------
@@ -167,11 +179,18 @@ net_error_t net_str2protocol(const char * str, net_protocol_id_t * id_ref)
 {
   net_protocol_id_t id= 0;
   while (id < NET_PROTOCOL_MAX) {
-    if (!strcmp(PROTOCOL_NAMES[id], str)) {
+    if (!strcmp(PROTOCOL_DEFS[id]->name, str)) {
       *id_ref= id;
       return ESUCCESS;
     }
     id++;
   }
   return ENET_PROTO_UNKNOWN;
+}
+
+// -----[ net_protocols_get_def ]----------------------------------
+const net_protocol_def_t * net_protocols_get_def(net_protocol_id_t id)
+{
+  assert(id < NET_PROTOCOL_MAX);
+  return PROTOCOL_DEFS[id];
 }
--- trunk/src/net/protocol.h
+++ trunk/src/net/protocol.h
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 25/02/2004
-// $Id: protocol.h,v 1.6 2008-06-13 14:26:23 bqu Exp $
+// $Id: protocol.h,v 1.7 2009-03-24 16:23:40 bqu Exp $
 // ==================================================================
 
 #ifndef __NET_PROTOCOL_H__
@@ -11,6 +11,8 @@
 
 #include <net/error.h>
 #include <net/net_types.h>
+
+extern const net_protocol_def_t * PROTOCOL_DEFS[NET_PROTOCOL_MAX];
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,23 +22,22 @@ extern "C" {
   net_error_t protocol_recv(net_protocol_t * proto, net_msg_t * msg,
 			    simulator_t * sim);
   
-  // ----- protocols_create -------------------------------------------
+  // -----[ protocols_create ]---------------------------------------
   net_protocols_t * protocols_create();
-  // ----- protocols_destroy ----------------------------------------
-  void protocols_destroy(net_protocols_t ** ppProtocols);
-  // ----- protocols_register ---------------------------------------
-  int protocols_register(net_protocols_t * pProtocols,
-			 net_protocol_id_t id,
-			 void * pHandler,
-			 FNetProtoHandlerDestroy fDestroy,
-			 FNetProtoHandleEvent fHandleEvent);
-  // ----- protocols_get --------------------------------------------
-  net_protocol_t * protocols_get(net_protocols_t * pProtocols,
+  // -----[ protocols_destroy ]--------------------------------------
+  void protocols_destroy(net_protocols_t ** protocols_ref);
+  // -----[ protocols_add ]------------------------------------------
+  int protocols_add(net_protocols_t * protocols, net_protocol_id_t id,
+		    void * handler);
+  // -----[ protocols_get ]------------------------------------------
+  net_protocol_t * protocols_get(net_protocols_t * protocols,
 				 net_protocol_id_t id);
   // -----[ net_protocol2str ]---------------------------------------
   const char * net_protocol2str(net_protocol_id_t id);
   // -----[ net_str2protocol ]---------------------------------------
-  net_error_t net_str2protocol(const char * str, net_protocol_id_t * pID);
+  net_error_t net_str2protocol(const char * str, net_protocol_id_t * id);
+  // -----[ net_protocols_get_def ]----------------------------------
+  const net_protocol_def_t * net_protocols_get_def(net_protocol_id_t id);
 
 #ifdef __cplusplus
 }
