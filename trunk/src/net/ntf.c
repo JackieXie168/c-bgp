8539cbea343c22f3525ed07c727c837cb3808cbd
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2009-03-24 16:08:30 +0000

    Changes: - cosmetic changes - NTF relies on lpr framework - documentation

--- trunk/src/net/ntf.c
+++ trunk/src/net/ntf.c
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 24/03/2005
-// $Id: ntf.c,v 1.10 2008-04-14 09:16:04 bqu Exp $
+// $Id: ntf.c,v 1.11 2009-03-24 16:08:30 bqu Exp $
 // ==================================================================
 
 #ifdef HAVE_CONFIG_H
@@ -13,135 +13,146 @@
 #include <assert.h>
 #include <stdio.h>
 
-#include <libgds/log.h>
-#include <libgds/tokenizer.h>
+#include <libgds/stream.h>
 
 #include <net/network.h>
 #include <net/node.h>
 #include <net/ntf.h>
 #include <net/util.h>
+#include <util/lrp.h>
+
+
+// -----[ MAX_NTF_LINE_LEN ]-----------------------------------------
+/** Maximum number of characters in an NTF line.
+ *
+ * The minimum space needed is:
+ *   = len(addr1)+1+len(addr2)+1+len(weight)+1+len(delay)+CR+LF+\0
+ *   = 15+1+15+1+10+1+10+1+1+1
+ *   = 56
+ */
+#define MAX_NTF_LINE_LEN 80
+
+// -----[ NTF_FIELD_DELIMITERS ]-------------------------------------
+#define NTF_FIELD_DELIMITERS " \t"
+
+
+static lrp_t * _parser= NULL;
+
+
+// -----[ _ntf_add_link ]--------------------------------------------
+static inline
+void _ntf_add_link(network_t * network,
+		   net_addr_t addr1, net_addr_t addr2,
+		   igp_weight_t weight, net_link_delay_t delay)
+{
+  net_node_t * node1, * node2;
+  net_iface_t * iface;
+
+  node1= network_find_node(network, addr1);
+  if (node1 == NULL) {
+    assert(node_create(addr1, &node1, NODE_OPTIONS_LOOPBACK) == ESUCCESS);
+    assert(network_add_node(network, node1) == ESUCCESS);
+  }
+  
+  node2= network_find_node(network, addr2);
+  if (node2 == NULL) {
+    assert(node_create(addr2, &node2, NODE_OPTIONS_LOOPBACK) == ESUCCESS);
+    assert(network_add_node(network, node2) == ESUCCESS);
+  }
+    
+  assert(net_link_create_rtr(node1, node2, BIDIR, &iface) == ESUCCESS);
+  assert(net_link_set_phys_attr(iface, delay, 0, BIDIR) == ESUCCESS);
+  assert(net_iface_set_metric(iface, 0, weight, BIDIR) == ESUCCESS);
+}
 
 // -----[ ntf_load ]-------------------------------------------------
-/**
- * Load the given NTF file into the topology.
- */
-int ntf_load(const char * file_name)
+int ntf_load(const char * filename)
 {
-  FILE * pFile;
-  char acFileLine[80];
-  int iError= NTF_SUCCESS;
-  STokenizer * pTokenizer;
-  STokens * pTokens;
-  uint32_t uLineNumber= 0;
-  char * pcValue;
-  net_addr_t tNode1, tNode2;
-  unsigned int uWeight;
-  unsigned int uDelay;
-  net_link_delay_t tDelay;
-  net_node_t * pNode1, * pNode2;
-  net_iface_t * pIface;
+  unsigned int num_fields;
+  const char * field;
+  net_addr_t addr1, addr2;
+  igp_weight_t weight;
+  net_link_delay_t delay;
   network_t * network= network_get_default();
 
-  pTokenizer= tokenizer_create(" \t", 0, NULL, NULL);
+  if (lrp_open(_parser, filename) < 0)
+    return lrp_error(_parser);
 
-  if ((pFile= fopen(file_name, "r")) != NULL) {
+  while (lrp_get_next_line(_parser)) {
+    if (lrp_get_num_fields(_parser, &num_fields) < 0)
+      break;
     
-    while ((!feof(pFile)) && (iError == NTF_SUCCESS)) {
-      if (fgets(acFileLine, sizeof(acFileLine), pFile) == NULL)
-	break;
-
-      uLineNumber++;
-      
-      if (tokenizer_run(pTokenizer, acFileLine)) {
-	iError= NTF_ERROR_UNEXPECTED;
-	LOG_ERR(LOG_LEVEL_SEVERE, "Error: unexpected parse error in NTF file, line %u\n",
-		   uLineNumber);
-	break;
-      }
-
-      pTokens= tokenizer_get_tokens(pTokenizer);
-
-      if (tokens_get_num(pTokens) < 3) {
-	LOG_ERR(LOG_LEVEL_SEVERE, "Error: not enough parameters in NTF file, line %u\n",
-		   uLineNumber);
-	iError= NTF_ERROR_NUM_PARAMS;
-	break;	
-      }
-
-      // Check IP address of first node
-      pcValue= tokens_get_string_at(pTokens, 0);
-      if (str2address(pcValue, &tNode1) < 0) {
-	LOG_ERR(LOG_LEVEL_SEVERE, "Error: invalid IP address for first node (%s), line %u\n",
-		   pcValue, uLineNumber);
-	iError= NTF_ERROR_INVALID_ADDRESS;
-	break;
-      }
-
-      // Check IP address of second node
-      pcValue= tokens_get_string_at(pTokens, 1);
-      if (str2address(pcValue, &tNode2) < 0) {
-	LOG_ERR(LOG_LEVEL_SEVERE, "Error: invalid IP address for second node (%s), line %u\n",
-		   pcValue, uLineNumber);
-	iError= NTF_ERROR_INVALID_ADDRESS;
+    // Check number of fields
+    if ((num_fields < 3) || (num_fields > 4)) {
+      lrp_set_user_error(_parser, "3 or 4 fields expected");
+      break;
+    }
+    
+    // Check IP address of first node
+    field= lrp_get_field(_parser, 0);
+    if (str2address(field, &addr1) < 0) {
+      lrp_set_user_error(_parser, "invalid source \"%s\"", field);
+      break;
+    }
+    
+    // Check IP address of second node
+    field= lrp_get_field(_parser, 1);
+    if (str2address(field, &addr2) < 0) {
+      lrp_set_user_error(_parser, "invalid destination \"%s\"", field);
+      break;
+    }
+    
+    // Check IGP metric
+    field= lrp_get_field(_parser, 2);
+    if (str2weight(field, &weight)) {
+      lrp_set_user_error(_parser, "invalid IGP metric (%s)", field);
+      break;
+    }
+    
+    // Check delay (if provided)
+    delay= 0;
+    if (num_fields == 4) {
+      field= lrp_get_field(_parser, 3);
+      if (str2delay(field, &delay)) {
+	lrp_set_user_error(_parser, "invalid delay (%s)", field);
 	break;
       }
-
-      // Check IGP metric
-      if (tokens_get_uint_at(pTokens, 2, &uWeight)) {
-	LOG_ERR(LOG_LEVEL_SEVERE, "Error: invalid IGP metric (%s), line %u\n",
-		   tokens_get_string_at(pTokens, 2), uLineNumber);
-	iError= NTF_ERROR_INVALID_WEIGHT;
-	break;
-      }
-      
-      // Check delay (if provided)
-      tDelay= (net_link_delay_t) uWeight;
-      if (tokens_get_num(pTokens) > 3) {
-	if (tokens_get_uint_at(pTokens, 3, &uDelay)) {
-	  LOG_ERR(LOG_LEVEL_SEVERE, "Error: invalid delay (%s), line %u\n",
-		     tokens_get_string_at(pTokens, 3), uLineNumber);
-	  iError= NTF_ERROR_INVALID_DELAY;
-	  break;
-	}
-        tDelay= (net_link_delay_t) uDelay;
-      }
-
-      pNode1= network_find_node(network, tNode1);
-      if (pNode1 == NULL) {
-	assert(node_create(tNode1, &pNode1, NODE_OPTIONS_LOOPBACK) == ESUCCESS);
-	assert(network_add_node(network, pNode1) == ESUCCESS);
-      }
-
-      pNode2= network_find_node(network, tNode2);
-      if (pNode2 == NULL) {
-	assert(node_create(tNode2, &pNode2, NODE_OPTIONS_LOOPBACK) == ESUCCESS);
-	assert(network_add_node(network, pNode2) == ESUCCESS);
-      }
-
-      assert(net_link_create_rtr(pNode1, pNode2, BIDIR, &pIface)
-	     == ESUCCESS);
-      assert(net_link_set_phys_attr(pIface, tDelay, 0, BIDIR) == ESUCCESS);
-      assert(net_iface_set_metric(pIface, 0, uWeight, BIDIR) == ESUCCESS);
     }
 
-    fclose(pFile);
-
-  } else {
-    LOG_ERR(LOG_LEVEL_SEVERE, "Error: could not open NTF file \"%s\"\n",
-	    file_name);
-    iError= NTF_ERROR_OPEN;    
+    // Create link
+    _ntf_add_link(network, addr1, addr2, weight, delay);
   }
-
-  tokenizer_destroy(&pTokenizer);
-
-  return 0;
+  
+  lrp_close(_parser);
+  return lrp_error(_parser);
 }
 
 // -----[ ntf_save ]-------------------------------------------------
-/**
- * Save the topology in an NTF file.
- */
-int ntf_save(const char * file_name)
+int ntf_save(const char * filename)
 {
   return -1;
+}
+
+// -----[ ntf_strerror ]---------------------------------------------
+const char * ntf_strerror()
+{
+  return lrp_strerror(_parser);
+}
+
+// -----[ ntf_strerrorloc ]------------------------------------------
+const char * ntf_strerrorloc()
+{
+  return lrp_strerrorloc(_parser);
+}
+
+// -----[ _ntf_init ]------------------------------------------------
+void _ntf_init()
+{
+  _parser= lrp_create(MAX_NTF_LINE_LEN, NTF_FIELD_DELIMITERS);
+}
+
+// -----[ _ntf_done ]------------------------------------------------
+void _ntf_done()
+{
+  lrp_destroy(&_parser);
 }
