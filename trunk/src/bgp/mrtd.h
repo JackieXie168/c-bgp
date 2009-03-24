5a96b100cb5778fd05afa4f4bbd4ab33861d7c4b
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2009-03-24 14:27:18 +0000

    Changes: - cosmetic changes - fixed error codes (derived from lrp framework) - support empty local-pref (default value) - support empty MED (default value)

--- trunk/src/bgp/mrtd.h
+++ trunk/src/bgp/mrtd.h
@@ -3,7 +3,7 @@
 //
 // @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 20/02/2004
-// $Id: mrtd.h,v 1.9 2008-04-11 11:03:06 bqu Exp $
+// $Id: mrtd.h,v 1.10 2009-03-24 14:27:18 bqu Exp $
 // ==================================================================
 
 #ifndef __MRTD_H__
@@ -12,11 +12,11 @@
 #include <libgds/array.h>
 #include <libgds/types.h>
 
-#include <bgp/as_t.h>
+#include <bgp/types.h>
 #include <bgp/message.h>
-#include <bgp/peer_t.h>
 #include <bgp/routes_list.h>
 #include <bgp/route-input.h>
+#include <util/lrp.h>
 
 typedef uint8_t mrtd_input_t;
 
@@ -30,6 +30,25 @@ typedef uint8_t mrtd_input_t;
 #define MRT_FORMAT_ASCII  0
 #define MRT_FORMAT_BINARY 1
 
+// -----[ mrtd_error_code_t ]----------------------------------------
+typedef enum {
+  MRTD_SUCCESS            = LRP_SUCCESS,
+  MRTD_ERROR              = LRP_ERROR_USER,
+  MRTD_ERROR_SYNTAX       = LRP_ERROR_USER-1,
+  MRTD_INVALID_PEER_ADDR  = LRP_ERROR_USER-2,
+  MRTD_INVALID_PEER_ASN   = LRP_ERROR_USER-3,
+  MRTD_INVALID_ORIGIN     = LRP_ERROR_USER-4,
+  MRTD_INVALID_ASPATH     = LRP_ERROR_USER-5,
+  MRTD_INVALID_LOCALPREF  = LRP_ERROR_USER-6,
+  MRTD_INVALID_MED        = LRP_ERROR_USER-7,
+  MRTD_INVALID_COMMUNITIES= LRP_ERROR_USER-8,
+  MRTD_INVALID_RECORD_TYPE= LRP_ERROR_USER-9,
+  MRTD_INVALID_PREFIX     = LRP_ERROR_USER-10,
+  MRTD_INVALID_NEXTHOP    = LRP_ERROR_USER-11,
+  MRTD_MISSING_FIELDS     = LRP_ERROR_USER-12,
+  MRTD_INVALID_PROTOCOL   = LRP_ERROR_USER-13,
+} mrtd_error_code_t;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -39,17 +58,22 @@ extern "C" {
   ///////////////////////////////////////////////////////////////////
 
   // ----- mrtd_create_path -----------------------------------------
-  SBGPPath * mrtd_create_path(const char * path);
+  bgp_path_t * mrtd_create_path(const char * path);
   // ----- mrtd_route_from_line -------------------------------------
-  bgp_route_t * mrtd_route_from_line(const char * line,
-				     net_addr_t * peer_addr,
-				     unsigned int * peer_asn);
+  int mrtd_route_from_line(const char * line,
+			   net_addr_t * peer_addr,
+			   asn_t * peer_asn,
+			   bgp_route_t ** route_ref);
   // ----- mrtd_msg_from_line ---------------------------------------
-  bgp_msg_t * mrtd_msg_from_line(bgp_router_t * router, bgp_peer_t * peer,
-			       const char * line);
+  int mrtd_msg_from_line(bgp_router_t * router, bgp_peer_t * peer,
+			 const char * line, bgp_msg_t ** msg_ref);
   // ----- mrtd_ascii_load_routes -----------------------------------
-  int mrtd_ascii_load(const char * file_name, FBGPRouteHandler handler,
+  int mrtd_ascii_load(const char * filename, bgp_route_handler_f handler,
 		      void * ctx);
+  // -----[ mrtd_strerror ]------------------------------------------
+  const char * mrtd_strerror(int result);
+  // -----[ mrtd_perror ]--------------------------------------------
+  void mrtd_perror(gds_stream_t * stream, int result);
 
 
   ///////////////////////////////////////////////////////////////////
@@ -58,7 +82,7 @@ extern "C" {
 
 #ifdef HAVE_BGPDUMP
   // -----[ mrtd_binary_load ]---------------------------------------
-  int mrtd_binary_load(const char * file_name, FBGPRouteHandler handler,
+  int mrtd_binary_load(const char * file_name, bgp_route_handler_f handler,
 		       void * ctx);
 #endif
 
