c0ccb008bb34a3e96845c2b0236e2f1ee8e7865b
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2009-03-24 16:28:39 +0000

    Changes: - cosmetic changes - new utility functions

--- src/net/util.h
+++ src/net/util.h
@@ -4,9 +4,9 @@
 // Various network utility function: conversion from string to
 // address and so on...
 //
-// @author Bruno Quoitin (bqu@info.ucl.ac.be)
+// @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 23/01/2007
-// @lastdate 23/01/2007
+// $Id: util.h,v 1.2 2009-03-24 16:28:39 bqu Exp $
 // ==================================================================
 
 #ifndef __NET_UTIL_H__
@@ -14,17 +14,32 @@
 
 #include <net/prefix.h>
 #include <net/net_types.h>
+#include <bgp/types.h>
 
 #ifdef __cplusplus
 extern"C" {
 #endif
 
   // ----- str2address ----------------------------------------------
-  int str2address(char * pcValue, net_addr_t * pAddr);
-  // ----- str2prefix -------------------------------------------------
-  int str2prefix(char * pcValue, SPrefix * pPrefix);
-  // ----- str2tos ----------------------------------------------------
-  int str2tos(char * pcValue, net_tos_t * pTOS);
+  int str2address(const char * str, net_addr_t * addr);
+  // ----- str2prefix -----------------------------------------------
+  int str2prefix(const char * str, ip_pfx_t * prefix);
+  // ----- str2tos --------------------------------------------------
+  int str2tos(const char * str, net_tos_t * tos);
+  // -----[ str2depth ]----------------------------------------------
+  int str2depth(const char * str, uint8_t * depth);
+  // -----[ str2capacity ]-------------------------------------------
+  int str2capacity(const char * str, net_link_load_t * capacity);
+  // -----[ str2delay ]----------------------------------------------
+  int str2delay(const char * str, net_link_delay_t * delay);
+  // -----[ str2weight ]---------------------------------------------
+  int str2weight(const char * str, igp_weight_t * weight);
+  // -----[ str2domain_id ]------------------------------------------
+  int str2domain_id(const char * str, unsigned int * id);
+  // -----[ str2ttl ]------------------------------------------------
+  int str2ttl(const char * str, uint8_t * ttl);
+  // -----[ str2asn ]------------------------------------------------
+  int str2asn(const char * str, asn_t * asn);
 
 #ifdef __cplusplus
 }
