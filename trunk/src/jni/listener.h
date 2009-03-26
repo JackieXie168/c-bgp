ef375aefc4be2e333c79511de7d39b440d5b69f3
bqu <bqu@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2009-03-26 13:24:14 +0000

    Changes: - cosmetic changes - code to get JNI environment by attaching thread to Java VM moved   from jni_interface.c to jni_listener_get_env() - documentation

--- trunk/src/jni/listener.h
+++ trunk/src/jni/listener.h
@@ -3,15 +3,20 @@
 //
 // @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
 // @date 29/06/2007
-// $Id: listener.h,v 1.2 2008-05-20 12:11:38 bqu Exp $
+// $Id: listener.h,v 1.3 2009-03-26 13:24:14 bqu Exp $
 // ==================================================================
+
+/**
+ * \file
+ * Contain data structures and functions to manage Java listeners.
+ */
 
 #ifndef __JNI_LISTENER_H__
 #define __JNI_LISTENER_H__
 
 #include <jni.h>
 
-// -----[ SListenerCtx ]---------------------------------------------
+// -----[ jni_listener_t ]-------------------------------------------
 /**
  * Context structure used for Java callbacks (listeners)
  *
@@ -21,20 +26,52 @@
  */
 typedef struct {
   JavaVM * jVM;
-  jobject joListener;
-} SJNIListener;
+  jobject  joListener;
+} jni_listener_t;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
   // -----[ jni_listener_init ]--------------------------------------
-  void jni_listener_init(SJNIListener * pListener);
+  /**
+   * Initialize a JNI listener.
+   *
+   * \param listener is the JNI listener.
+   */
+  void jni_listener_init(jni_listener_t * listener);
+
   // -----[ jni_listener_set ]---------------------------------------
-  void jni_listener_set(SJNIListener * pListener, JNIEnv * jEnv,
-			jobject joListener);
+  /**
+   * Assign a Java object to a JNI listener.
+   *
+   * \param listener      is the JNI listener.
+   * \param env           is the JNI environment (used to get the
+   *                      Java VM reference).
+   * \param java_listener is the Java object listener.
+   */
+  void jni_listener_set(jni_listener_t * listener, JNIEnv * env,
+			jobject java_listener);
+
   // -----[ jni_listener_unset ]-------------------------------------
-  void jni_listener_unset(SJNIListener * pListener, JNIEnv * jEnv);
+  /**
+   * Clear the Java object assigned to a JNI listener.
+   *
+   * \param listener is the JNI listener.
+   * \param env      is the JNI environment.
+   */
+  void jni_listener_unset(jni_listener_t * listener, JNIEnv * env);
+
+  // -----[ jni_listener_get_env ]-----------------------------------
+  /**
+   * Attach the current thread to the Java VM specified in the
+   * listener object and get a pointer to the JNI environment.
+   *
+   * \param listener is the listener object.
+   * \param env_ref  is a reference to the JNI environment (used to
+   *                 retrieve the JNI environment).
+   */
+  void jni_listener_get_env(jni_listener_t * listener, JNIEnv ** env_ref);
 
 #ifdef __cplusplus
 }
