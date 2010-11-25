c59f14043a130d25cea1210480ae0483d15b8925
beauport <beauport@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2010-11-25 17:06:16 +0000

    begin of code of what a state in a graph is...

---
+++ branches/cbgp_stefan_sched/src/sim/state.c
@@ -0,0 +1,56 @@
+// ==================================================================
+// @(#)state.c
+//
+// @author Bruno Quoitin (bruno.quoitin@uclouvain.be)
+// @date 08/08/2005
+// $Id: node.c,v 1.19 2009-08-31 09:48:28 bqu Exp $
+// ==================================================================
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <assert.h>
+#include <libgds/str_util.h>
+
+#include <net/error.h>
+#include <net/icmp.h>
+#include <net/link.h>
+#include <net/link-list.h>
+#include <net/net_types.h>
+#include <net/netflow.h>
+#include <net/network.h>
+#include <net/node.h>
+#include <net/subnet.h>
+
+
+#include <state.h>
+
+
+// ----- state_create ------------------------------------------------
+routing_state_t * _routing_state_create(state_t * state)
+{
+  routing_state_t * routing_state;
+
+  routing_state= (routing_state *) MALLOC(sizeof(routing_state));
+
+  routing_state->state = state;
+  routing_state->couples_node_rib = (couple_node_rib_t **) MALLOC( state->graph->nb_nodes * sizeof(couple_node_rib_t *));
+
+
+
+  return state;
+}
+
+// ----- state_create ------------------------------------------------
+state_t * state_create(graph_t * graph)
+{
+  state_t * state;
+   
+  state=(state *) MALLOC(sizeof(state_t));
+  state->routing_state = _routing_state_create(state);
+  state->queue_state = _queue_state_create();
+  state->output_transitions = _output_transitions_create();
+
+  return state;
+}
