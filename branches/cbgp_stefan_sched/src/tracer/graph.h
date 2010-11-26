5bfbcecf10b5adefef962f6ee61abc8c7961d085
beauport <beauport@5148e334-2ac9-4710-8d03-e6f91fc789ed>
2010-11-26 11:41:45 +0000

    tracer

---
+++ branches/cbgp_stefan_sched/src/tracer/graph.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   graph.h
+ * Author: yo
+ *
+ * Created on 26 novembre 2010, 12:18
+ */
+
+#ifndef GRAPH_H
+#define	GRAPH_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* GRAPH_H */
+
---
+++ branches/cbgp_stefan_sched/src/tracer/tracer.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   tracer.h
+ * Author: yo
+ *
+ * Created on 26 novembre 2010, 12:16
+ */
+
+#ifndef __TRACER_H__
+#define	__TRACER_H__
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TRACER_H */
+
